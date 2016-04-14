def0f03015915db2cc3950a098b130310285c732
Austin Bean <austinbean@gmail.com>
2016-04-14 16:48:53 -0500

    PerturbAction.jl first draft works adequately

--- PerturbAction.jl
+++ PerturbAction.jl
@@ -1,16 +1,17 @@
 # PerturbAction
 using Distributions
+type VarianceException <: Exception end
 
-function perturb(probs::Array, eps::Float64, control::bool)
-  actions = size(probs)[1]
+function perturb(probs::Array, eps::Float64, control::Bool)
+  actions = maximum(size(probs)) # always a column vector
   if control == true
-    srand(123)
-  elseif control == false 
-    # nothing
+    srand(123) #enables debugging by using seed.
+  end
+  if (eps > 1) | (eps < 0)
+    return VarianceException
   end
 
   d = Normal(0, eps)
-  pturb = rand(d, actions)
-  probs = probs + pturb
-
+  pturb = rand(d, actions)'
+  return (max((probs + pturb),0))/(sum(max(probs + pturb, 0)))
 end
