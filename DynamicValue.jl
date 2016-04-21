d6f43e32cd112c9c5c0cbfb30d6cde355a382c10
Austin Bean <austinbean@gmail.com>
2016-04-21 13:28:18 -0500

    Changes to dynamic value.jl: produces matrix for single firm only

--- DynamicValue.jl
+++ DynamicValue.jl
@@ -1,30 +1,44 @@
-# I don't know if I actually need this at all.  State History already
-# includes  everything I want.
 
 
-function DynamicValue(state_history::Array, beta::Float64, T::Int64)
+function DynamicValue(state_history::Array, fac_fid::Float64; pat_types = 1, β = 0.95, T = 100, max_hosp = 25)
   len, width = size(state_history)
-  fac_section = state_history[:, width-4]
-  agg_section = state_history[:, width-3:end-1]
-  probs = state_history[:, end]
+  index = findfirst(state_history[1,:], fac_fid) # where is the perturbed facility
+  fac_section = state_history[:, index:index+6] # take the section from the actual history corresponding
+  agg_section = state_history[:, width-3:end]
+  history = hcat(fac_section, agg_section)
 
-  mtotal = maximum(sum(agg_section[:,1:3 ], 2)) #maximum total hospitals.
-  mlev1 = maximum(agg_section[:,1]) # maximum level 1
-  mlev2 = maximum(agg_section[:,2]) # maximum level 2
-  mlev3 = maximum(agg_section[:,3]) # maximum level 3
+#= Output format:
+ Row 1 : [0, 1, 2, ..., 25] - for level 1
+ Row 2 : [0, 1, 2, ..., 25] - for level 2
+ Row 3 : [0, 1, 2, ..., 25] - for level 3
 
-  fids = unique(convert(Array{Int}, state_history[1, 1:6:end-4] ))
-  numfids = size(fids)[1]
-  mkt_history = zeros(6*T ,numfids)
+- Each row element will be: Β^t * Demand * probability of visit
+- The choice of row depends on the level of the hospital's facility
+- The specific element of each row depends on how many facilities at the level there
+  are
+- Think ahead: if we do this separately for Medicaid patients, I'll need 6 rows since the per-patient
+  revenues are different
+- Entry and exit to be recorded in a separate manner.
+=#
 
-  for ind in size(fids)[1]
-    index = findfirst(state_history[1,:], fid[ind])
-    history_slice = state_history[:, index:index+5]
+  outp = zeros(3*pat_types, max_hosp + 1) # visits to max hospital or 0 at each of 3 levels
 
+# The last 4 columns of "history" or "state_history" are: level 1, level 2, level 3, probability
+# The seven elements of the records are: fid, act_solo, act_int, choice prob, action, demand and perturbation indicator
 
-
-
+  for row in 1:(T+1):
+    if (history[row,2], history[row,3]) == (0,0)
+      levelcount = history[row, end-3]
+      outp[3, levelcount+1] += (β^row)*history[row, 6]*history[row,end]
+    elseif (history[row,2], history[row,3]) == (1,0)
+      levelcount = history[row, end-2]
+      outp[2, levelcount+1] += (β^row)*history[row, 6]*history[row,end]
+    elseif (history[row,2], history[row,3]) == (0,1)
+      levelcount = history[row, end-1]
+      outp[1, levelcount+1] += (β^row)*history[row, 6]*history[row,end]
+    end
   end
 
+return outp
 
 end
