379ff466219cd84d1812bf178556fee572a73c9a
Austin Bean <austinbean@gmail.com>
2016-05-19 21:22:26 -0500

    OptimResults improved - keeps only non-zero columns and includes parameter labels

--- OptimResults.jl
+++ OptimResults.jl
@@ -4,6 +4,7 @@ using DataFrames
 using DataArrays
 using Distributions
 using Optim
+using PyPlot
 
 
 fout1 = readtable("/Users/austinbean/Desktop/dynhosp/simulationresults.csv")
@@ -34,6 +35,32 @@ end
 
 names!(fout1, colnames)
 
+paramsymbs = Array{UTF8String}(0)
+for k = 1:3
+  for i = 0:25
+    push!(paramsymbs, "Θ"*"$k"*"C$i")
+  end
+end
+for y in [1 2 3]
+  for z in [1 2 3 "EX"]
+    if y != z
+      push!(paramsymbs, "ψ"*"$y"*"$z")
+    end
+  end
+end
+
+push!(paramsymbs, "Γ1")
+push!(paramsymbs, "Γ2")
+push!(paramsymbs, "Γ3")
+
+for i in 1:size(paramsymbs)[1]
+  print(colnames[3+i],"  ", paramsymbs[i], "\n")
+end
+
+
+
+
+
 function dfvec(datafr::DataFrame)
   vecvals = Vector{Float64}(0)
   for el in 1:size(datafr)[2]
@@ -44,26 +71,34 @@ end
 
 # Delete columns of zeros - this is just for the testing part.  Eventually hopefully all will be filled in.
 # This only deletes if both the column for the equilibrium AND non-equilibrium are zero.
-
-for el in 1:size(colnames[4:end-90])[1]
+# this doesn't work because the size is changing dynamically.
+deletdsym = Array{Int64}(0)
+deletdcols = Array{Int64}(0)
+for col in colnames[4:end-90]
+  el = fout1.colindex.lookup[col] #gets column number from dictionary
+  nel = el + 90
   if (sum(fout1[colnames[el]]) == 0) & (sum(fout1[colnames[el+90]]) == 0)
-    print("Empty Column: ", colnames[el], " ", colnames[el+90], "\n")
-    delete!(fout1, colnames[el])
-    delete!(fout1, colnames[el+90])
+    print("Empty Column: ", colnames[el], " ", colnames[el+90], " Symbol:", paramsymbs[el-3] ,"\n")
+    push!(deletdcols, el)
+    push!(deletdcols, nel)
+    push!(deletdsym, el-3)
   end
 end
-
-# Check resulting:
-for el in fout1.colindex.names
-  print(el, "\n")
-end
+deleteat!(paramsymbs, deletdsym)
+delete!(fout1, deletdcols)
 
 # Number of simulations and num
 hsims = size(fout1)[1] # number of simulations
 ncols = size(fout1)[2] # number of columns with nonzeros (pairs!)
 
-# How many parameters am I trying to estimate?  # of Non-zero column pairs 
+# How many parameters am I trying to estimate?  # of Non-zero column pairs
 params = convert(Int, (ncols-3)/2) # don't think this conversion is strictly necessary
+
+# Check resulting:
+for el in 1:params
+  print(fout1.colindex.names[el+3], "  ", paramsymbs[el], "\n")
+end
+
 
 
 for x in 1:hsims
@@ -92,7 +127,19 @@ result = optimize(sumval, ones(params), method = SimulatedAnnealing(), iteration
 
 result = optimize(sumval, ones(params), method = SimulatedAnnealing(), iterations = 50000, store_trace = true)
 
+result2 = optimize(sumval, ones(params), method = SimulatedAnnealing(), iterations = 5000, extended_trace = true, store_trace = true);
 
+result = optimize(sumval, 500*ones(params), method = SimulatedAnnealing(), iterations = 50000, store_trace = true, ftol = 1.0e-8)
+
+
+# Now this will print the parameter name:
+for el in 1:convert(Int, (size(fout1.colindex.names)[1]-3)/2)
+  print(fout1.colindex.names[el+3], "  ", Optim.minimizer(result)[el], " param: ", paramsymbs[el], "\n")
+end
+
+# These are not very informative yet.  
+plot(Optim.minimizer(result)[1:4], paramsymbs[1:4])
+plot(Optim.minimizer(result)[5:8], paramsymbs[5:8])
 
 
 ##
