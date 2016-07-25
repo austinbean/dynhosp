212aac87ebe2011c7330e958fc68ebe67ee97ebc
Austin Bean <austinbean@gmail.com>
2016-07-24 19:54:55 -0500

    Some changes - main to function but no faster

--- Main.jl
+++ Main.jl
@@ -14,7 +14,9 @@ Platform Info:
 include("/Users/austinbean/Desktop/dynhosp/Main.jl")
 =#
 
-
+# Benchmark: not function - 498.977793 seconds (1.97 G allocations: 220.996 GB, 23.46% gc time)
+# In function: 503.579793 seconds (2.24 G allocations: 230.951 GB, 23.54% gc time)
+# In a function combining all of what's in Reboot.jl: 529.434186 seconds (2.23 G allocations: 228.223 GB, 23.71% gc time)
 
 
 ### Collect Basic Information ###
@@ -25,136 +27,152 @@ include("/Users/austinbean/Desktop/dynhosp/Main.jl")
 
 # Next one stores all the years appearing in each fips code
 #yearins = [ [x; findfirst(data[:,fipscodeloc], x); findlast(data[:,fipscodeloc], x ); unique( data[findfirst(data[:,fipscodeloc], x):findlast(data[:,fipscodeloc], x ) , yearloc]  ) ] for x in unique(data[:,fipscodeloc])  ];
+function main1()
 
-yearins = [ x   for x in unique(data[ data[:,yearloc].==2005 , fipscodeloc])]
-
-
-container = zeros(1, ((2*78+12)*2)+3) #must agree with outp in ParMainFun
-
-# Open the existing saved data:
-fout1 = DataFrames.readtable(pathprograms*"simulationresults.csv")
-donefips  = [x for x in unique(fout1[!(DataFrames.isna(fout1[:fipscode])),:fipscode])] # take codes NOT done before
-#donefips = [] # set this to be empty until the simulation gets working.
+  yearins = [ x   for x in unique(data[ data[:,yearloc].==2005 , fipscodeloc])]
 
 
-colnames = Array{Symbol}(:0)
-push!(colnames, :fipscode)
-push!(colnames, :fid)
-push!(colnames, :year)
-for elem in ["EQ", "NEQ"]
-  for name in ["PI", "MED"]
-    for j = 1:3
-      for k = 0:25
-        push!(colnames, parse("$name""$elem"*"Lev$j"*"Comp$k"))
+  container = zeros(1, ((2*78+12)*2)+3) #must agree with outp in ParMainFun
+
+  # Open the existing saved data:
+  fout1 = DataFrames.readtable(pathprograms*"simulationresults.csv")
+  donefips  = [x for x in unique(fout1[!(DataFrames.isna(fout1[:fipscode])),:fipscode])] # take codes NOT done before
+  #donefips = [] # set this to be empty until the simulation gets working.
+
+
+  colnames = Array{Symbol}(:0)
+  push!(colnames, :fipscode)
+  push!(colnames, :fid)
+  push!(colnames, :year)
+  for elem in ["EQ", "NEQ"]
+    for name in ["PI", "MED"]
+      for j = 1:3
+        for k = 0:25
+          push!(colnames, parse("$name""$elem"*"Lev$j"*"Comp$k"))
+        end
       end
     end
-  end
-  for x in [1 2 3]
-    for y in [1 2 3 "EX"]
-      if x != y
-        push!(colnames, parse("$elem"*"Trans$x$y"))
+    for x in [1 2 3]
+      for y in [1 2 3 "EX"]
+        if x != y
+          push!(colnames, parse("$elem"*"Trans$x$y"))
+        end
       end
     end
+    push!(colnames, parse("$elem"*"Enter1"))
+    push!(colnames, parse("$elem"*"Enter2"))
+    push!(colnames, parse("$elem"*"Enter3"))
   end
-  push!(colnames, parse("$elem"*"Enter1"))
-  push!(colnames, parse("$elem"*"Enter2"))
-  push!(colnames, parse("$elem"*"Enter3"))
-end
 
-monopoly = Array{Int64}(0)
-duopoly = Array{Int64}(0)
-triopoly = Array{Int64}(0)
-tetrapoly = Array{Int64}(0)
-nopoly = Array{Int64}(0)
-all = Array{Int64}(0)
+  monopoly = Array{Int64}(0)
+  duopoly = Array{Int64}(0)
+  triopoly = Array{Int64}(0)
+  tetrapoly = Array{Int64}(0)
+  nopoly = Array{Int64}(0)
+  all = Array{Int64}(0)
 
-for el in yearins
-  unqfids = [x for x in unique(data[ (data[:,fipscodeloc].==el[1])&(data[:,yearloc].==2005) ,fidloc])]
-  if size(unqfids,1) == 1
-  #  print("Fipscode Monopoly: ", unique(dataf[el[2]:el[3], :fipscode]), "\n")
-    println("Fipscode Monopoly: ", el[1])
-    println(unqfids)
-    push!(monopoly, el[1])
-  elseif size(unqfids,1) == 2
-    println("Fipscode Duopoly: ", el[1])
-    push!(duopoly, el[1])
-  elseif size(unqfids,1) == 3
-    println("Fipscode Triopoly: ", el[1])
-    push!(triopoly, el[1])
-  elseif size(unqfids,1) == 4
-    println("Fipscode Tetrapoly: ", el[1])
-    push!(tetrapoly, el[1])
-  elseif size(unqfids,1) > 4
-    println("Fipscode N-opoly: ", el[1])
-    println("Fipscode Hospitals: ", size(unqfids, 1))
-    push!(nopoly, el[1])
-  end
-  push!(all, size(unqfids,1))
-end
-
-timestamps = Array{Any}(0)
-
-for y in 1:1#size(nopoly,1)
-    mkt_fips = duopoly[y][1]
-    crtime = now()
-    timestr = Dates.format(crtime, "yyyy-mm-dd HH:MM:ss")
-    push!(timestamps, (mkt_fips, "begin", timestr))
-    if !(in(mkt_fips, donefips)) & (mkt_fips != 48201) & (mkt_fips != 0) # this is going to do new fipscodes only
-      print("Market FIPS Code ", mkt_fips, "\n")
-      	for year in [ 2005 ]   #yearins[y][4:end] # can do all years or several.
-          fids =  sort!(convert(Array{Int64}, unique(data[(data[:,fipscodeloc].==mkt_fips)&(data[:, yearloc].==year),fidloc])))
-          # This will parallelize the computation across Monte Carlo sims.
-          mcres = @parallel (+) for i = 1:5
-    #                println(i)
-                    ParMainfun(data, pinsured, privatedemandmodelparameters, pmedicaid, medicaiddemandmodelparameters, mkt_fips, year,  fids; npers = 50)
-                  end
-          # the addition map adds together years, fipscodes and fids.  Divide by nsims to recover.
-          mcres[:, 1:3] = mcres[:,1:3]/500
-          container = [container; mcres]
-          println("Terminated fips: ", mkt_fips)
-      end #Note - rewrite first line of state history back to peoples.
-    #  dat = 0
+  for el in yearins
+    unqfids = [x for x in unique(data[ (data[:,fipscodeloc].==el[1])&(data[:,yearloc].==2005) ,fidloc])]
+    if size(unqfids,1) == 1
+    #  print("Fipscode Monopoly: ", unique(dataf[el[2]:el[3], :fipscode]), "\n")
+      println("Fipscode Monopoly: ", el[1])
+      println(unqfids)
+      push!(monopoly, el[1])
+    elseif size(unqfids,1) == 2
+      println("Fipscode Duopoly: ", el[1])
+      push!(duopoly, el[1])
+    elseif size(unqfids,1) == 3
+      println("Fipscode Triopoly: ", el[1])
+      push!(triopoly, el[1])
+    elseif size(unqfids,1) == 4
+      println("Fipscode Tetrapoly: ", el[1])
+      push!(tetrapoly, el[1])
+    elseif size(unqfids,1) > 4
+      println("Fipscode N-opoly: ", el[1])
+      println("Fipscode Hospitals: ", size(unqfids, 1))
+      push!(nopoly, el[1])
     end
-    # Record what time certain ends happened.
-      outf = open(pathprograms*"timer.txt", "w")
+    push!(all, size(unqfids,1))
+  end
+
+  timestamps = Array{Any}(0)
+
+  for y in 1:1#size(nopoly,1)
+      mkt_fips = duopoly[y][1]
       crtime = now()
       timestr = Dates.format(crtime, "yyyy-mm-dd HH:MM:ss")
-      push!(timestamps, (mkt_fips, "end", timestr))
-      #print(timestamps, "\n")
-      writedlm(outf, timestamps)
-      close(outf)
-    # Write out temporary files in case the process fails later.
-      tout = convert(DataFrame, container);
-      names!(tout, colnames)
-      tout = tout[ tout[:fipscode].>0, :]
-      DataFrames.writetable(pathprograms*"temp_results_$mkt_fips.csv", tout) # no slash.
-end
-
-println("Terminated Estimation")
-
-# Add column names to the new data:
-output1 = convert(DataFrame, container);
-names!(output1, colnames)
-output1 = output1[ output1[:fipscode].>0 ,:]
-
-# Append new data to the existing data - there is a stupid problem here that the types don't match exactly.  Make everything Float64:
-for el in names(fout1)
-  if (typeof(fout1[el]) == DataArrays.DataArray{Int64,1})
-    fout1[DataFrames.isna(fout1[el]),:] = -666
-  elseif (typeof(fout1[el]) == DataArrays.DataArray{Float64,1})
-    fout1[DataFrames.isna(fout1[el]),:] = -666.0
-  else
-#    print("other type ", typeof(el), "\n")
+      push!(timestamps, (mkt_fips, "begin", timestr))
+      if !(in(mkt_fips, donefips)) & (mkt_fips != 48201) & (mkt_fips != 0) # this is going to do new fipscodes only
+        print("Market FIPS Code ", mkt_fips, "\n")
+        	for year in [ 2005 ]   #yearins[y][4:end] # can do all years or several.
+            fids =  sort!(convert(Array{Int64}, unique(data[(data[:,fipscodeloc].==mkt_fips)&(data[:, yearloc].==year),fidloc])))
+            # This will parallelize the computation across Monte Carlo sims.
+            mcres = @parallel (+) for i = 1:5
+      #                println(i)
+                      ParMainfun(data, pinsured, privatedemandmodelparameters, pmedicaid, medicaiddemandmodelparameters, mkt_fips, year,  fids; npers = 50)
+                    end
+            # the addition map adds together years, fipscodes and fids.  Divide by nsims to recover.
+            mcres[:, 1:3] = mcres[:,1:3]/500
+            container = [container; mcres]
+            println("Terminated fips: ", mkt_fips)
+        end #Note - rewrite first line of state history back to peoples.
+      #  dat = 0
+      end
+      # Record what time certain ends happened.
+        outf = open(pathprograms*"timer.txt", "w")
+        crtime = now()
+        timestr = Dates.format(crtime, "yyyy-mm-dd HH:MM:ss")
+        push!(timestamps, (mkt_fips, "end", timestr))
+        #print(timestamps, "\n")
+        writedlm(outf, timestamps)
+        close(outf)
+      # Write out temporary files in case the process fails later.
+        tout = convert(DataFrame, container);
+        names!(tout, colnames)
+        tout = tout[ tout[:fipscode].>0, :]
+        DataFrames.writetable(pathprograms*"temp_results_$mkt_fips.csv", tout) # no slash.
   end
-end
 
-for el in names(fout1)
-  fout1[el] = convert( Array{Float64,1}, fout1[el])
-end
+  println("Terminated Estimation")
 
-append!(fout1, output1)
-fout1 = fout1[fout1[:fipscode].!=-666,:]
-writetable(pathprograms*"simulationresults.csv", output1)
+  # Add column names to the new data:
+  output1 = convert(DataFrame, container);
+  names!(output1, colnames)
+  output1 = output1[ output1[:fipscode].>0 ,:]
+
+  # Append new data to the existing data - there is a stupid problem here that the types don't match exactly.  Make everything Float64:
+  for el in names(fout1)
+    if (typeof(fout1[el]) == DataArrays.DataArray{Int64,1})
+      fout1[DataFrames.isna(fout1[el]),:] = -666
+    elseif (typeof(fout1[el]) == DataArrays.DataArray{Float64,1})
+      fout1[DataFrames.isna(fout1[el]),:] = -666.0
+    else
+  #    print("other type ", typeof(el), "\n")
+    end
+  end
+
+  for el in names(fout1)
+    fout1[el] = convert( Array{Float64,1}, fout1[el])
+  end
+
+  append!(fout1, output1)
+  fout1 = fout1[fout1[:fipscode].!=-666,:]
+  writetable(pathprograms*"simulationresults.csv", output1)
+
+end #of function main1()
+
+main1()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -163,66 +181,21 @@ writetable(pathprograms*"simulationresults.csv", output1)
 
 #=
 
-=#
+  allindices = [ (x, findfirst(data[:,fipscodeloc], x), findlast(data[:,fipscodeloc], x)) for x in unique(data[:,fipscodeloc]) ];
+
+  # Action codes:
+
+  # 1 "To 3 from 1"
+  # 2 "To 2 from 1"
+  # 3 "To 2 from 3"
+  # 4 "To 1 from 3"
+  # 5 "To 1 from 2"
+  # 6 "To 3 from 2"
+  # 7 "Enter at 1"
+  # 8 "Enter at 2"
+  # 9 "Enter at 3"
+  # 10 "Do Nothing"
+  # 11 "Exit"
 
 
-
-
-
-
-
-
-
-
-
-###
-
-
-
-#=
-
-allindices = [ (x, findfirst(data[:,fipscodeloc], x), findlast(data[:,fipscodeloc], x)) for x in unique(data[:,fipscodeloc]) ];
-
-
-# Data Types - currently unused.
-
-type Hosp
-	fid::Int
-	name::AbstractString
-	level::Tuple
-	fips::Int
-	lat::Float64
-	long::Float64
-	n05::Array
-	n515::Array
-	n1525::Array
-	choices::Array
-	probs::WeightVec
-end
-
-h1 = Hosp(dataf[1,:fid], dataf[1,:facility], (dataf[1,:act_int], dataf[1,:act_solo]), dataf[1,:fipscode], dataf[1, :v15], dataf[1, :v16],     [dataf[1,:lev105], dataf[1,:lev205], dataf[1,:lev305]], [dataf[1,:lev1515], dataf[1,:lev2515], dataf[1,:lev3515]], [dataf[1,:lev11525], dataf[1,:lev21525], dataf[1,:lev31525]], [1, 2, 3, 4], WeightVec([0.1, 0.1, 0.1, 0.1])     )
-
-type Market
-	fips::Int
-	lev1::Int
-	lev2::Int
-	lev3::Int
-	config::Array{Hosp}
-end
-
-
-
-# Action codes:
-#=
-1 "To 3 from 1"
-2 "To 2 from 1"
-3 "To 2 from 3"
-4 "To 1 from 3"
-5 "To 1 from 2"
-6 "To 3 from 2"
-7 "Enter at 1"
-8 "Enter at 2"
-9 "Enter at 3"
-10 "Do Nothing"
-11 "Exit"
 =#
--- OptimResults.jl
+++ OptimResults.jl
@@ -1,4 +1,6 @@
 # Take the outcome of main and give it to the optimizer:
+# include("/Users/austinbean/Desktop/dynhosp/OptimResults.jl")
+
 
 using DataFrames
 using Distributions
@@ -169,7 +171,8 @@ function mainopt()
   # result = optimize(objfun_2, 500*ones(params), method = SimulatedAnnealing(), iterations = 50000, store_trace = true)
   # This runs very quickly, even with 500,000 evaluations.
   # store_trace
-  result3 = optimize(objfun, 1000*ones(params), method = SimulatedAnnealing(), iterations = 1_000_000, show_trace = true, show_every = 100000)
+  const starting = 3500
+  result3 = optimize(objfun, starting*ones(params), method = SimulatedAnnealing(), iterations = 3_000_000, show_trace = true, show_every = 100000)
 
 
   # Now this will print the parameter name:
@@ -178,13 +181,20 @@ function mainopt()
   end
 
   x1 = paramsymbs[1:19]
-  p1 = plot(x=x1, y=Optim.minimizer(result3)[1:19], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 1 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line ) # the collect[1:6] is the NUMBER of parameters, not their indices
-
   x2 = paramsymbs[21:40]
-  p2 = plot(x=x2, y=Optim.minimizer(result3)[21:40], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 2 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line  )
-
   x3 = paramsymbs[42:61]
-  p3 = plot(x=x3, y=Optim.minimizer(result3)[42:61], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 3 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line  )
+  # p1 = plot(
+  #           layer(x=x1, y=Optim.minimizer(result3)[1:19], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 1 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line, Theme(default_color=colorant"green")),
+  #           layer(x=x2, y=Optim.minimizer(result3)[21:40], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 2 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line, Theme(default_color=colorant"purple")),
+  #           layer(x=x3, y=Optim.minimizer(result3)[42:61], Guide.xticks(ticks=collect(1:19)), Guide.xlabel("Profits Per Patient at Level 3 \n Given Competitors C#"), Guide.ylabel("Dollars"), Geom.point, Geom.line, Theme(default_color=colorant"red"))
+  #           )
+  p1 = plot(
+            layer(x=x1, y=Optim.minimizer(result3)[1:19], Geom.point, Geom.line, Theme(default_color=colorant"green")),
+            layer(x=x2, y=Optim.minimizer(result3)[21:40], Geom.point, Geom.line, Theme(default_color=colorant"purple")),
+            layer(x=x3, y=Optim.minimizer(result3)[42:61], Geom.point, Geom.line, Theme(default_color=colorant"red")),
+            layer(x=x1, y=starting*ones(size(Optim.minimizer(result3)[1:19])), Geom.line, Theme(default_color=colorant"black")),
+            Guide.xlabel("Number of Competitors"), Guide.ylabel("Dollars"), Guide.manual_color_key("Levels", ["Level 1", "Level 2", "Level 3", "Initial Value"], ["green", "purple", "red", "black"])
+            )
 
 
 end #of mainopt() function
@@ -278,73 +288,73 @@ Outcomes:
 # directly via the matrix, I don't need to do that.
 
 # This is used below in function definitions
-function dfvec(datafr::DataFrame)
-  vecvals = Vector{Float64}(0)
-  for el in 1:size(datafr,2)
-    push!(vecvals, datafr[el].data[1])
-  end
-  return vecvals
-end
-function dfvec(datafr::Array{Real,2})
-  vecvals = Vector{Float64}(0)
-  for el in 1:size(datafr,2)
-    push!(vecvals, datafr[el].data[1])
-  end
-  return vecvals
-end
-
-# older set of column names:
-
-colnames = Array{Symbol}(:0)
-push!(colnames, :fipscode)
-push!(colnames, :fid)
-push!(colnames, :year)
-for elem in ["EQ", "NEQ"]
-  for name in ["PI", "MED"]
-    for j = 1:3
-      for k = 0:25
-        push!(colnames, parse("$name""$elem"*"Lev$j"*"Comp$k"))
-      end
-    end
-  end
-  for x in [1 2 3]
-    for y in [1 2 3 "EX"]
-      if x != y
-        push!(colnames, parse("$elem"*"Trans$x$y"))
-      end
-    end
-  end
-  push!(colnames, parse("$elem"*"Enter1"))
-  push!(colnames, parse("$elem"*"Enter2"))
-  push!(colnames, parse("$elem"*"Enter3"))
-end
-
-# Relics from an earlier version when I treated each row separately (very, very slow)
-
-
-# How many parameters am I trying to estimate?  # of Non-zero column pairs
-params = convert(Int, (ncols-3)/2) # don't think this conversion is strictly necessary
-
-fout1 = fout1[:,4:end]
-for x in 1:size(fout1,1)
-  name = parse("function val$x(x::Vector; inp1 = fout1[$x, 1:params-1], inp2 = fout1[$x, params:end]) return (minimum([sum(x.*(inp1 - inp2)), 0.0]))^2 end")
-  eval(name)
-end
-
-
-#To evaluate the above:
-for x in 1:20
- phrs = parse("val$x(ones(params))")
- print(eval(phrs), "\n")
-end
-
-str = ""
-for x in 1:size(fout1,1)
- str = str*"val$x(x) + "
-end
-
-function sumval(x::Vector; hsims = 500)
-  return (1/hsims)*(val1(x) + val2(x) + val3(x) + val4(x) + val5(x) + val6(x) + val7(x) + val8(x) + val9(x) + val10(x) + val11(x) + val12(x) + val13(x) + val14(x) + val15(x) + val16(x) + val17(x) + val18(x) + val19(x) + val20(x) + val21(x) + val22(x) + val23(x) + val24(x) + val25(x) + val26(x) + val27(x) + val28(x) + val29(x) + val30(x) + val31(x) + val32(x) + val33(x) + val34(x) + val35(x) + val36(x) + val37(x) + val38(x) + val39(x) + val40(x) + val41(x) + val42(x) + val43(x) + val44(x) + val45(x) + val46(x) + val47(x) + val48(x) + val49(x) + val50(x) + val51(x) + val52(x) + val53(x) + val54(x) + val55(x) + val56(x) + val57(x) + val58(x) + val59(x) + val60(x) + val61(x) + val62(x) + val63(x) + val64(x) + val65(x) + val66(x) + val67(x) + val68(x) + val69(x) + val70(x) + val71(x) + val72(x) + val73(x) + val74(x) + val75(x) + val76(x) + val77(x) + val78(x) + val79(x) + val80(x) + val81(x) + val82(x) + val83(x) + val84(x) + val85(x) + val86(x) + val87(x) + val88(x) + val89(x) + val90(x) + val91(x) + val92(x) + val93(x) + val94(x) + val95(x) + val96(x) + val97(x) + val98(x) + val99(x) + val100(x) + val101(x) + val102(x) + val103(x) + val104(x) + val105(x) + val106(x) + val107(x) + val108(x) + val109(x) + val110(x) + val111(x) + val112(x) + val113(x) + val114(x) + val115(x) + val116(x) + val117(x) + val118(x) + val119(x) + val120(x) + val121(x) + val122(x) + val123(x) + val124(x) + val125(x) + val126(x) + val127(x) + val128(x) + val129(x) + val130(x) + val131(x) + val132(x) + val133(x) + val134(x) + val135(x) + val136(x) + val137(x) + val138(x) + val139(x) + val140(x) + val141(x) + val142(x) + val143(x) + val144(x) + val145(x) + val146(x) + val147(x) + val148(x) + val149(x) + val150(x) + val151(x) + val152(x) + val153(x) + val154(x) + val155(x) + val156(x) + val157(x) + val158(x) + val159(x) + val160(x) + val161(x) + val162(x) + val163(x) + val164(x) + val165(x) + val166(x) + val167(x) + val168(x) + val169(x) + val170(x) + val171(x) + val172(x) + val173(x) + val174(x) + val175(x) + val176(x) + val177(x) + val178(x) + val179(x) + val180(x) + val181(x) + val182(x) + val183(x) + val184(x) + val185(x) + val186(x) + val187(x) + val188(x) + val189(x) + val190(x) + val191(x) + val192(x) + val193(x) + val194(x) + val195(x) + val196(x) + val197(x) + val198(x) + val199(x) + val200(x) + val201(x) + val202(x) + val203(x) + val204(x) + val205(x) + val206(x) + val207(x) + val208(x) + val209(x) + val210(x))
-end
+# function dfvec(datafr::DataFrame)
+#   vecvals = Vector{Float64}(0)
+#   for el in 1:size(datafr,2)
+#     push!(vecvals, datafr[el].data[1])
+#   end
+#   return vecvals
+# end
+# function dfvec(datafr::Array{Real,2})
+#   vecvals = Vector{Float64}(0)
+#   for el in 1:size(datafr,2)
+#     push!(vecvals, datafr[el].data[1])
+#   end
+#   return vecvals
+# end
+#
+# # older set of column names:
+#
+# colnames = Array{Symbol}(:0)
+# push!(colnames, :fipscode)
+# push!(colnames, :fid)
+# push!(colnames, :year)
+# for elem in ["EQ", "NEQ"]
+#   for name in ["PI", "MED"]
+#     for j = 1:3
+#       for k = 0:25
+#         push!(colnames, parse("$name""$elem"*"Lev$j"*"Comp$k"))
+#       end
+#     end
+#   end
+#   for x in [1 2 3]
+#     for y in [1 2 3 "EX"]
+#       if x != y
+#         push!(colnames, parse("$elem"*"Trans$x$y"))
+#       end
+#     end
+#   end
+#   push!(colnames, parse("$elem"*"Enter1"))
+#   push!(colnames, parse("$elem"*"Enter2"))
+#   push!(colnames, parse("$elem"*"Enter3"))
+# end
+#
+# # Relics from an earlier version when I treated each row separately (very, very slow)
+#
+#
+# # How many parameters am I trying to estimate?  # of Non-zero column pairs
+# params = convert(Int, (ncols-3)/2) # don't think this conversion is strictly necessary
+#
+# fout1 = fout1[:,4:end]
+# for x in 1:size(fout1,1)
+#   name = parse("function val$x(x::Vector; inp1 = fout1[$x, 1:params-1], inp2 = fout1[$x, params:end]) return (minimum([sum(x.*(inp1 - inp2)), 0.0]))^2 end")
+#   eval(name)
+# end
+#
+#
+# #To evaluate the above:
+# for x in 1:20
+#  phrs = parse("val$x(ones(params))")
+#  print(eval(phrs), "\n")
+# end
+#
+# str = ""
+# for x in 1:size(fout1,1)
+#  str = str*"val$x(x) + "
+# end
+#
+# function sumval(x::Vector; hsims = 500)
+#   return (1/hsims)*(val1(x) + val2(x) + val3(x) + val4(x) + val5(x) + val6(x) + val7(x) + val8(x) + val9(x) + val10(x) + val11(x) + val12(x) + val13(x) + val14(x) + val15(x) + val16(x) + val17(x) + val18(x) + val19(x) + val20(x) + val21(x) + val22(x) + val23(x) + val24(x) + val25(x) + val26(x) + val27(x) + val28(x) + val29(x) + val30(x) + val31(x) + val32(x) + val33(x) + val34(x) + val35(x) + val36(x) + val37(x) + val38(x) + val39(x) + val40(x) + val41(x) + val42(x) + val43(x) + val44(x) + val45(x) + val46(x) + val47(x) + val48(x) + val49(x) + val50(x) + val51(x) + val52(x) + val53(x) + val54(x) + val55(x) + val56(x) + val57(x) + val58(x) + val59(x) + val60(x) + val61(x) + val62(x) + val63(x) + val64(x) + val65(x) + val66(x) + val67(x) + val68(x) + val69(x) + val70(x) + val71(x) + val72(x) + val73(x) + val74(x) + val75(x) + val76(x) + val77(x) + val78(x) + val79(x) + val80(x) + val81(x) + val82(x) + val83(x) + val84(x) + val85(x) + val86(x) + val87(x) + val88(x) + val89(x) + val90(x) + val91(x) + val92(x) + val93(x) + val94(x) + val95(x) + val96(x) + val97(x) + val98(x) + val99(x) + val100(x) + val101(x) + val102(x) + val103(x) + val104(x) + val105(x) + val106(x) + val107(x) + val108(x) + val109(x) + val110(x) + val111(x) + val112(x) + val113(x) + val114(x) + val115(x) + val116(x) + val117(x) + val118(x) + val119(x) + val120(x) + val121(x) + val122(x) + val123(x) + val124(x) + val125(x) + val126(x) + val127(x) + val128(x) + val129(x) + val130(x) + val131(x) + val132(x) + val133(x) + val134(x) + val135(x) + val136(x) + val137(x) + val138(x) + val139(x) + val140(x) + val141(x) + val142(x) + val143(x) + val144(x) + val145(x) + val146(x) + val147(x) + val148(x) + val149(x) + val150(x) + val151(x) + val152(x) + val153(x) + val154(x) + val155(x) + val156(x) + val157(x) + val158(x) + val159(x) + val160(x) + val161(x) + val162(x) + val163(x) + val164(x) + val165(x) + val166(x) + val167(x) + val168(x) + val169(x) + val170(x) + val171(x) + val172(x) + val173(x) + val174(x) + val175(x) + val176(x) + val177(x) + val178(x) + val179(x) + val180(x) + val181(x) + val182(x) + val183(x) + val184(x) + val185(x) + val186(x) + val187(x) + val188(x) + val189(x) + val190(x) + val191(x) + val192(x) + val193(x) + val194(x) + val195(x) + val196(x) + val197(x) + val198(x) + val199(x) + val200(x) + val201(x) + val202(x) + val203(x) + val204(x) + val205(x) + val206(x) + val207(x) + val208(x) + val209(x) + val210(x))
+# end
 
 =#
