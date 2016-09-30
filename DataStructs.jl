9a4073b46749f10ae77ef3b749349c0527638d5f
Austin Bean <austinbean@gmail.com>
2016-09-29 20:22:03 -0500

    Fixed a bunch of stupid mistakes in CondSum

--- DataStructs.jl
+++ DataStructs.jl
@@ -981,7 +981,7 @@ end
 
 #     Texas = MakeNew(fips, data05); # New state every time - this is kind of inefficient.
 
-#  Tex2 = NewSim(30, Texas, patients);
+#  Tex2 = NewSim(3, Texas, patients);
 #  EmpTex = CreateEmpty(fips, data05);
 
 function Termination(EmTex::EntireState)
@@ -1075,7 +1075,6 @@ function PSim(T::Int, pats::patientcollection; di = data05, fi = fips, entrants 
     termflag = !Termination(EmptyState)                                                               # Checks the termination condition over every market in the state.
     counter += 1
   end # of while
-  println("Iteration Count: ", counter)
   return EmptyState
 end
 
@@ -1231,85 +1230,83 @@ function ResultsOut(Tex::EntireState, OtherTex::EntireState; T::Int64 = 50, beta
   for el in 1:size(fids,1)
     outp[el,1] = fids[el]                                                           # Write out all of the fids as an ID in the first column.
   end
-  for el in keys(Tex.fipsdirectory)                                                 # Now this is all of the hospitals.
+  for el in keys(Tex.fipsdirectory)                                                 # Now this is all of the hospitals by FID
     hosp = Tex.mkts[Tex.fipsdirectory[el]].collection[el]
     outprob = prod(hosp.probhistory)                                                # Prob of the outcome.
     private, medicaid, wtp_out, transitions = CondSum(hosp)
     arr = zeros(1, 40)
-    arr[1] = (alph1 = (beta^T)*outprob*dot(wtp_out[1:7], private[1:7])  )           # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 1
-    arr[2] = (alph2 = (beta^T)*outprob*dot(wtp_out[8:14], private[8:14])   )        # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 2
-    arr[3] = (alph3 = (beta^T)*outprob*dot(wtp_out[15:21], private[15:21]) )        # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 3
-    arr[4] = (gamma_1_385 = (beta^T)*outprob*(private[1]+medicaid[1])   )           # The next lines are patients summed over types.  Costs are treated as the same over Medicaid and privately insured.
-    arr[5] = (gamma_2_385 = (beta^T)*outprob*(private[8]+medicaid[8]) )             # I don't use any of the named terms - they just keep track.
-    arr[6] = (gamma_3_385 = (beta^T)*outprob*(private[15]+medicaid[15]) )
-    arr[7] = (gamma_1_386 = (beta^T)*outprob*(private[2]+medicaid[2]) )
-    arr[8] = (gamma_2_386 = (beta^T)*outprob*(private[9]+medicaid[9]) )
-    arr[9] = (gamma_3_386 = (beta^T)*outprob*(private[16]+medicaid[16]) )
-    arr[10] = (gamma_1_387 = (beta^T)*outprob*(private[3]+medicaid[3]) )
-    arr[11] = (gamma_2_387 = (beta^T)*outprob*(private[10]+medicaid[10]) )
-    arr[12] = (gamma_3_387 = (beta^T)*outprob*(private[17]+medicaid[17]) )
-    arr[13] = (gamma_1_388 = (beta^T)*outprob*(private[4]+medicaid[4]) )
-    arr[14] = (gamma_2_388 = (beta^T)*outprob*(private[11]+medicaid[11]) )
-    arr[15] = (gamma_3_388 = (beta^T)*outprob*(private[18]+medicaid[18]) )
-    arr[16] = (gamma_1_389 = (beta^T)*outprob*(private[5]+medicaid[5]) )
-    arr[17] = (gamma_2_389 = (beta^T)*outprob*(private[12]+medicaid[12]) )
-    arr[18] = (gamma_3_389 = (beta^T)*outprob*(private[19]+medicaid[19]) )
-    arr[19] = (gamma_1_390 = (beta^T)*outprob*(private[6]+medicaid[6]) )
-    arr[20] = (gamma_2_390 = (beta^T)*outprob*(private[13]+medicaid[13]) )
-    arr[21] = (gamma_3_390 = (beta^T)*outprob*(private[20]+medicaid[20]) )
-    arr[22] = (gamma_1_391 = (beta^T)*outprob*(private[7]+medicaid[7]) )
-    arr[23] = (gamma_2_391 = (beta^T)*outprob*(private[14]+medicaid[14]) )
-    arr[24] = (gamma_3_391 = (beta^T)*outprob*(private[21]+medicaid[21]) )
-    arr[25] = (patients385 = (beta^T)*outprob*(medicaid[1]+medicaid[8]+medicaid[15]))    # Count of patients at DRG 385
-    arr[26] = (patients386 = (beta^T)*outprob*(medicaid[2]+medicaid[9]+medicaid[16]))
-    arr[27] = (patients387 = (beta^T)*outprob*(medicaid[3]+medicaid[10]+medicaid[17]))
-    arr[28] = (patients388 = (beta^T)*outprob*(medicaid[4]+medicaid[11]+medicaid[18]))
-    arr[29] = (patients389 = (beta^T)*outprob*(medicaid[5]+medicaid[12]+medicaid[19]))
-    arr[30] = (patients390 = (beta^T)*outprob*(medicaid[6]+medicaid[13]+medicaid[20]))
-    arr[31] = (patients391 = (beta^T)*outprob*(medicaid[7]+medicaid[14]+medicaid[21]))
+    arr[1] = (beta^T)*outprob*dot(wtp_out[1:7], private[1:7])            # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 1
+    arr[2] = (beta^T)*outprob*dot(wtp_out[8:14], private[8:14])           # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 2
+    arr[3] = (beta^T)*outprob*dot(wtp_out[15:21], private[15:21])         # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 3
+    arr[4] = (beta^T)*outprob*(private[1]+medicaid[1])              # The next lines are patients summed over types.  Costs are treated as the same over Medicaid and privately insured.
+    arr[5] = (beta^T)*outprob*(private[8]+medicaid[8])              # I don't use any of the named terms - they just keep track.
+    arr[6] = (beta^T)*outprob*(private[15]+medicaid[15])
+    arr[7] = (beta^T)*outprob*(private[2]+medicaid[2])
+    arr[8] = (beta^T)*outprob*(private[9]+medicaid[9])
+    arr[9] = (beta^T)*outprob*(private[16]+medicaid[16])
+    arr[10] = (beta^T)*outprob*(private[3]+medicaid[3])
+    arr[11] = (beta^T)*outprob*(private[10]+medicaid[10])
+    arr[12] = (beta^T)*outprob*(private[17]+medicaid[17])
+    arr[13] = (beta^T)*outprob*(private[4]+medicaid[4])
+    arr[14] = (beta^T)*outprob*(private[11]+medicaid[11])
+    arr[15] = (beta^T)*outprob*(private[18]+medicaid[18])
+    arr[16] = (beta^T)*outprob*(private[5]+medicaid[5])
+    arr[17] = (beta^T)*outprob*(private[12]+medicaid[12])
+    arr[18] = (beta^T)*outprob*(private[19]+medicaid[19])
+    arr[19] = (beta^T)*outprob*(private[6]+medicaid[6])
+    arr[20] = (beta^T)*outprob*(private[13]+medicaid[13])
+    arr[21] = (beta^T)*outprob*(private[20]+medicaid[20])
+    arr[22] = (beta^T)*outprob*(private[7]+medicaid[7])
+    arr[23] = (beta^T)*outprob*(private[14]+medicaid[14])
+    arr[24] = (beta^T)*outprob*(private[21]+medicaid[21])
+    arr[25] = (beta^T)*outprob*(medicaid[1]+medicaid[8]+medicaid[15])         # Count of patients at DRG 385
+    arr[26] = (beta^T)*outprob*(medicaid[2]+medicaid[9]+medicaid[16])
+    arr[27] = (beta^T)*outprob*(medicaid[3]+medicaid[10]+medicaid[17])
+    arr[28] = (beta^T)*outprob*(medicaid[4]+medicaid[11]+medicaid[18])
+    arr[29] = (beta^T)*outprob*(medicaid[5]+medicaid[12]+medicaid[19])
+    arr[30] = (beta^T)*outprob*(medicaid[6]+medicaid[13]+medicaid[20])
+    arr[31] = (beta^T)*outprob*(medicaid[7]+medicaid[14]+medicaid[21])
     arr[32:end] = (alltrans = (beta^T)*outprob*transitions)
-    index = findfirst(outp[:,1], hosp.fid)                                          # find where the fid is in the list.
+    index = findfirst(outp[:,1], hosp.fid)                                    # find where the fid is in the list.
     outp[index, 2:41] = arr
-  end
-  for el in keys(OtherTex.fipsdirectory)
-    hosp = Tex.mkts[Tex.fipsdirectory[el]].collection[el]
-    outprob = prod(hosp.probhistory)                                                # Prob of the outcome.
-    private, medicaid, wtp_out, transitions = CondSum(hosp)
-    arr = zeros(1, 40)
-    arr[1] = (alph1 = (beta^T)*outprob*dot(wtp_out[1:7], private[1:7])  )           # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 1
-    arr[2] = (alph2 = (beta^T)*outprob*dot(wtp_out[8:14], private[8:14])   )        # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 2
-    arr[3] = (alph3 = (beta^T)*outprob*dot(wtp_out[15:21], private[15:21]) )        # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 3
-    arr[4] = (gamma_1_385 = (beta^T)*outprob*(private[1]+medicaid[1])   )           # The next lines are patients summed over types.  Costs are treated as the same over Medicaid and privately insured.
-    arr[5] = (gamma_2_385 = (beta^T)*outprob*(private[8]+medicaid[8]) )             # I don't use any of the named terms - they just keep track.
-    arr[6] = (gamma_3_385 = (beta^T)*outprob*(private[15]+medicaid[15]) )
-    arr[7] = (gamma_1_386 = (beta^T)*outprob*(private[2]+medicaid[2]) )
-    arr[8] = (gamma_2_386 = (beta^T)*outprob*(private[9]+medicaid[9]) )
-    arr[9] = (gamma_3_386 = (beta^T)*outprob*(private[16]+medicaid[16]) )
-    arr[10] = (gamma_1_387 = (beta^T)*outprob*(private[3]+medicaid[3]) )
-    arr[11] = (gamma_2_387 = (beta^T)*outprob*(private[10]+medicaid[10]) )
-    arr[12] = (gamma_3_387 = (beta^T)*outprob*(private[17]+medicaid[17]) )
-    arr[13] = (gamma_1_388 = (beta^T)*outprob*(private[4]+medicaid[4]) )
-    arr[14] = (gamma_2_388 = (beta^T)*outprob*(private[11]+medicaid[11]) )
-    arr[15] = (gamma_3_388 = (beta^T)*outprob*(private[18]+medicaid[18]) )
-    arr[16] = (gamma_1_389 = (beta^T)*outprob*(private[5]+medicaid[5]) )
-    arr[17] = (gamma_2_389 = (beta^T)*outprob*(private[12]+medicaid[12]) )
-    arr[18] = (gamma_3_389 = (beta^T)*outprob*(private[19]+medicaid[19]) )
-    arr[19] = (gamma_1_390 = (beta^T)*outprob*(private[6]+medicaid[6]) )
-    arr[20] = (gamma_2_390 = (beta^T)*outprob*(private[13]+medicaid[13]) )
-    arr[21] = (gamma_3_390 = (beta^T)*outprob*(private[20]+medicaid[20]) )
-    arr[22] = (gamma_1_391 = (beta^T)*outprob*(private[7]+medicaid[7]) )
-    arr[23] = (gamma_2_391 = (beta^T)*outprob*(private[14]+medicaid[14]) )
-    arr[24] = (gamma_3_391 = (beta^T)*outprob*(private[21]+medicaid[21]) )
-    arr[25] = (patients385 = (beta^T)*outprob*(medicaid[1]+medicaid[8]+medicaid[15]))
-    arr[26] = (patients386 = (beta^T)*outprob*(medicaid[2]+medicaid[9]+medicaid[16]))
-    arr[27] = (patients387 = (beta^T)*outprob*(medicaid[3]+medicaid[10]+medicaid[17]))
-    arr[28] = (patients388 = (beta^T)*outprob*(medicaid[4]+medicaid[11]+medicaid[18]))
-    arr[29] = (patients389 = (beta^T)*outprob*(medicaid[5]+medicaid[12]+medicaid[19]))
-    arr[30] = (patients390 = (beta^T)*outprob*(medicaid[6]+medicaid[13]+medicaid[20]))
-    arr[31] = (patients391 = (beta^T)*outprob*(medicaid[7]+medicaid[14]+medicaid[21]))
-    arr[32:end] = (alltrans = (beta^T)*outprob*transitions)
-    index = findfirst(outp[:,1], hosp.fid)                                             # find where the fid is in the list.
-    outp[index, 42:end] = arr
+    # NB: Here starts the second state record.
+    hosp_neq = OtherTex.mkts[OtherTex.fipsdirectory[el]].collection[el]       # Find the record in the OTHER EntireState
+    outprobn = prod(hosp_neq.probhistory)                                     # Prob of the outcome.
+    privaten, medicaidn, wtp_outn, transitionsn = CondSum(hosp_neq)
+    narr = zeros(1, 40)
+    narr[1] = (beta^T)*outprobn*dot(wtp_outn[1:7], privaten[1:7])             # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 1
+    narr[2] = (beta^T)*outprobn*dot(wtp_outn[8:14], privaten[8:14])           # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 2
+    narr[3] = (beta^T)*outprobn*dot(wtp_outn[15:21], privaten[15:21])         # This is WTP over all DRGS * patient vols at corresponding DRG, over all periods at level 3
+    narr[4] = (beta^T)*outprobn*(privaten[1]+medicaidn[1])                    # The next lines are patients summed over types.  Costs are treated as the same over Medicaid and privately insured.
+    narr[5] = (beta^T)*outprobn*(privaten[8]+medicaidn[8])
+    narr[6] = (beta^T)*outprobn*(privaten[15]+medicaidn[15])
+    narr[7] = (beta^T)*outprobn*(privaten[2]+medicaidn[2])
+    narr[8] = (beta^T)*outprobn*(privaten[9]+medicaidn[9])
+    narr[9] = (beta^T)*outprobn*(privaten[16]+medicaidn[16])
+    narr[10] = (beta^T)*outprobn*(privaten[3]+medicaidn[3])
+    narr[11] = (beta^T)*outprobn*(privaten[10]+medicaidn[10])
+    narr[12] = (beta^T)*outprobn*(privaten[17]+medicaidn[17])
+    narr[13] = (beta^T)*outprobn*(privaten[4]+medicaidn[4])
+    narr[14] = (beta^T)*outprobn*(privaten[11]+medicaidn[11])
+    narr[15] = (beta^T)*outprobn*(privaten[18]+medicaidn[18])
+    narr[16] = (beta^T)*outprobn*(privaten[5]+medicaidn[5])
+    narr[17] = (beta^T)*outprobn*(privaten[12]+medicaidn[12])
+    narr[18] = (beta^T)*outprobn*(privaten[19]+medicaidn[19])
+    narr[19] = (beta^T)*outprobn*(privaten[6]+medicaidn[6])
+    narr[20] = (beta^T)*outprobn*(privaten[13]+medicaidn[13])
+    narr[21] = (beta^T)*outprobn*(privaten[20]+medicaidn[20])
+    narr[22] = (beta^T)*outprobn*(privaten[7]+medicaidn[7])
+    narr[23] = (beta^T)*outprobn*(privaten[14]+medicaidn[14])
+    narr[24] = (beta^T)*outprobn*(privaten[21]+medicaidn[21])
+    narr[25] = (beta^T)*outprobn*(medicaidn[1]+medicaidn[8]+medicaidn[15])
+    narr[26] = (beta^T)*outprobn*(medicaidn[2]+medicaidn[9]+medicaidn[16])
+    narr[27] = (beta^T)*outprobn*(medicaidn[3]+medicaidn[10]+medicaidn[17])
+    narr[28] = (beta^T)*outprobn*(medicaidn[4]+medicaidn[11]+medicaidn[18])
+    narr[29] = (beta^T)*outprobn*(medicaidn[5]+medicaidn[12]+medicaidn[19])
+    narr[30] = (beta^T)*outprobn*(medicaidn[6]+medicaidn[13]+medicaidn[20])
+    narr[31] = (beta^T)*outprobn*(medicaidn[7]+medicaidn[14]+medicaidn[21])
+    narr[32:end] = (beta^T)*outprobn*transitionsn
+    outp[index, 42:end] = narr
   end
   return sortrows(outp, by=x->x[1])                                                    # sort by first column (fid)
 end
@@ -1318,12 +1315,14 @@ end
 function OuterSim(MCcount::Int; T1::Int64 = 3, dim1::Int64 = 290, dim2::Int64 = 67, fi = fips, da = data05)
   # Runs the Monte Carlo - Equilibrium and Non-equilibrium simulations for each market MCcount times.
   # Note that the reduction is (+), but that includes adding the fids, so this must be divided by MCcount
-  # to return correct results.  I think it gives an error if there are no other workers.
+  # to return correct results.
+  #outp = Array{Float64,2}()
   outp = @parallel (+) for j = 1:MCcount
     println("Current iteration ", j)
     Texas = MakeNew(fi, da);                                                                            # very quick ≈ 0.1 seconds.
-    patients = NewPatients()
-    ResultsOut(NewSim(T1, Texas, patients), PSim(T1, patients); T = T1)                                 # simulates and writes out the results.
+    eq_patients = NewPatients()                                                                         # Separate patients.
+    neq_patients = NewPatients()                                                                        # These need separate patient groups
+    ResultsOut(NewSim(T1, Texas, eq_patients), PSim(T1, neq_patients); T = T1)                                 # simulates and writes out the results.
   end
   outp[:,1] = outp[:,1]/MCcount                                                                         # Combined by (+) so reproduce the fids by dividing.
   return outp
