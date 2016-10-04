2d2bf54e129344a4551b0ffd2552f28924f74da8
Austin Bean <austinbean@gmail.com>
2016-10-04 08:31:53 -0500

    Reimbursement average computations

--- DRGReimb.py
+++ DRGReimb.py
@@ -11,9 +11,11 @@ with open('/Users/austinbean/Desktop/justreimb.csv', encoding = "ISO-8859-1") as
 
 nams = set()
 drgs = set()
+sts = set()
 for row in drgdata:
 	nams.add(row[2])
 	drgs.add(row[9])
+	sts.add(row[6])
 
 #drgs = 385, 386, 387, 388, 389, 390, 391
 #drgs = 789, 790, 791, 792, 793, 794, 795
@@ -30,7 +32,7 @@ pats2012 = [0, 0, 0, 0, 0, 0, 0]
 chrg2012 = [0, 0, 0, 0, 0, 0, 0]
 
 for row in drgdata:
-	if row[2][0:8] == "HOSPITAL":
+	if (("HOSPITAL" in row[2])|("HEALTH CLINIC" in row[2]))&(row[6] == "TX"):
 		if '>' in row[14]:
 			row[14] = row[14].replace('>', '').strip()
 		elif '<' in row[14]:
@@ -41,115 +43,115 @@ for row in drgdata:
 		if eval(row[9]) == 789:
 			if '2008' in row[8]:
 				pats2008[0] += eval(row[14].replace(',','').strip())
-				chrg2008[0] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[0] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[0] += eval(row[14].replace(',','').strip())
-				chrg2009[0] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[0] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[0] += eval(row[14].replace(',','').strip())
-				chrg2010[0] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[0] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[0] += eval(row[14].replace(',','').strip())
-				chrg2011[0] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[0] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[0] += eval(row[14].replace(',','').strip())
-				chrg2012[0] += eval(row[15].replace('$','').replace(',','').strip())				
+				chrg2012[0] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())				
 		elif eval(row[9]) == 790:
 			if '2008' in row[8]:
 				pats2008[1] += eval(row[14].replace(',','').strip())
-				chrg2008[1] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[1] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[1] += eval(row[14].replace(',','').strip())
-				chrg2009[1] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[1] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[1] += eval(row[14].replace(',','').strip())
-				chrg2010[1] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[1] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[1] += eval(row[14].replace(',','').strip())
-				chrg2011[1] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[1] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[1] += eval(row[14].replace(',','').strip())
-				chrg2012[1] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[1] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 		elif eval(row[9]) == 791:
 			if '2008' in row[8]:
 				pats2008[2] += eval(row[14].replace(',','').strip())
-				chrg2008[2] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[2] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[2] += eval(row[14].replace(',','').strip())
-				chrg2009[2] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[2] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[2] += eval(row[14].replace(',','').strip())
-				chrg2010[2] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[2] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[2] += eval(row[14].replace(',','').strip())
-				chrg2011[2] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[2] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[2] += eval(row[14].replace(',','').strip())
-				chrg2012[2] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[2] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 		elif eval(row[9]) == 792:
 			if '2008' in row[8]:
 				pats2008[3] += eval(row[14].replace(',','').strip())
-				chrg2008[3] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[3] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[3] += eval(row[14].replace(',','').strip())
-				chrg2009[3] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[3] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[3] += eval(row[14].replace(',','').strip())
-				chrg2010[3] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[3] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[3] += eval(row[14].replace(',','').strip())
-				chrg2011[3] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[3] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[3] += eval(row[14].replace(',','').strip())
-				chrg2012[3] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[3] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 		elif eval(row[9]) == 793:
 			if '2008' in row[8]:
 				pats2008[4] += eval(row[14].replace(',','').strip())
-				chrg2008[4] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[4] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[4] += eval(row[14].replace(',','').strip())
-				chrg2009[4] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[4] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[4] += eval(row[14].replace(',','').strip())
-				chrg2010[4] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[4] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[4] += eval(row[14].replace(',','').strip())
-				chrg2011[4] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[4] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[4] += eval(row[14].replace(',','').strip())
-				chrg2012[4] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[4] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 		elif eval(row[9]) == 794:
 			if '2008' in row[8]:
 				pats2008[5] += eval(row[14].replace(',','').strip())
-				chrg2008[5] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[5] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[5] += eval(row[14].replace(',','').strip())
-				chrg2009[5] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[5] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[5] += eval(row[14].replace(',','').strip())
-				chrg2010[5] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[5] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[5] += eval(row[14].replace(',','').strip())
-				chrg2011[5] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[5] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[5] += eval(row[14].replace(',','').strip())
-				chrg2012[5] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[5] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 		elif eval(row[9]) == 795:
 			if '2008' in row[8]:
 				pats2008[6] += eval(row[14].replace(',','').strip())
-				chrg2008[6] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2008[6] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2009' in row[8]:
 				pats2009[6] += eval(row[14].replace(',','').strip())
-				chrg2009[6] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2009[6] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2010' in row[8]:
 				pats2010[6] += eval(row[14].replace(',','').strip())
-				chrg2010[6] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2010[6] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2011' in row[8]:
 				pats2011[6] += eval(row[14].replace(',','').strip())
-				chrg2011[6] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2011[6] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 			elif '2012' in row[8]:
 				pats2012[6] += eval(row[14].replace(',','').strip())
-				chrg2012[6] += eval(row[15].replace('$','').replace(',','').strip())
+				chrg2012[6] += eval(row[14].replace(',','').strip())*eval(row[15].replace('$','').replace(',','').strip())
 
 
 
