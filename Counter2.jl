f283eaf027ffb914c607d1d2b9c0f5eb876a54fa
Austin Bean <austinbean@gmail.com>
2016-10-23 20:01:55 -0500

    Starting the dynamic one

--- Counter2.jl
+++ Counter2.jl
@@ -1,1 +1,36 @@
 # Now solve the dynamic model itself.
+
+
+# Can put these into a market and then into a state.
+
+
+type simh<:Fac
+  level::Int64
+  wvalues::Array{Float64,1}
+  actprobs::Array{Float64,1}
+  exit::Bool
+end
+
+
+# The "Payoff" function in counter 1 gets the profit
+
+function ActionValue(hos::simh, pats::Dict{Int64, patientcount})
+
+
+
+  return
+end
+
+
+
+function ActionProbs(hos::simh)
+
+
+  return 
+end
+
+
+function CheckConvergence()
+
+
+end
