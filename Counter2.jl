57ad5378b67bf6b74bdfec91f2ba9e6266b7e453
Austin Bean <austinbean@gmail.com>
2016-10-24 16:28:38 -0500

    Further work - problem in creation of structure

--- Counter2.jl
+++ Counter2.jl
@@ -5,36 +5,123 @@
 
 # use the type neighbors !
 
-type state
+using Distributions
+using DataFrames
+type hitcount # this type will record visits to a state-action pair
+  conf::neighbors
+  visits::Dict{Int64,Int64}
+end
+
+type history
+  path::Dict{neighbors, hitcount}
+  totalcount::Int64 # records total number of iterations
+end
+
+
+# Initialize empty:  hithist = history(Dict{neighbors, hitcount}(), 0)
+
+type hstate
+  #NB: track these for the level !
   conf::neighbors
   wvalues::Array{Float64,1}
   actprobs::Array{Float64,1}
 end
 
-#=
-function Base.isequal(n1::neighbors, n2::neighbors)::Bool
-  n1.level105 == n2.level105 && n1.level205 == n2.level205 && n1.level305 == n2.level305 && n1.level1515 == n2.level1515 && n1.level2515 == n2.level2515 && n1.level3515 == n2.level3515 && n1.level11525 == n2.level11525 && n1.level21525 == n2.level21525 && n1.level31525 == n2.level31525
+type shortrec<:ProjectModule.Fac
+  # needs to take location.  must measure distances.  need to update all of these every time, probably.
+  fid::Int64
+  lat::Float64
+  long::Float64
+  level::Int64
+  truelevel::Int64
+  ns::neighbors
+  choices::Array{Int64, 2}
+  chprobs::WeightVec
 end
 
-function Base.hash(n1::neighbors)
-  hash((n1.level105, n1.level205, n1.level305, n1.level1515, n1.level2515, n1.level3515, n1.level11525, n1.level21525, n1.level31525) )
-end
-=#
 
-type simh<:Fac
+type simh<:ProjectModule.Fac
+  fid::Int64
   level::Int64
   actual::Int64
-  visited::Dict{neighbors, state}
+  visited::Dict{neighbors, hstate} #possible given "isequal" and "hash" extended for "neighbors"
+  ns::Array{shortrec, 1}
   exit::Bool
 end
 
-#NB: create the state with the values.  Need good starting values
 
-DynTex = EntireState(Array{Market,1}(), Dict{Int64, Market}(), Dict{Int64, Int64}())
+type DynState # this should hold a collection of ALL of the hospitals, for the approximation.
+  all::Array{simh, 1}
+end
 
-function DynStateCreate( ; fi = ProjectModule.fips)
 
 
+"""
+`DynStateCreate(Tex::EntireState)`
+Create the dynamic records from the existing state, don't bother doing it from scratch.
+And these don't have to be organized by zip.
+Use the EntireState from the equilibrium simulation, not the first counterfactual.
+Make using
+TexasEq = MakeNew(ProjectModule.fips, ProjectModule.data05)
+"""
+function DynStateCreate( Tex::EntireState )
+  outp = DynState(Array{simh,1}())
+  for k1 in keys(Tex.mkts)
+    for hk in keys(Tex.mkts[k1].collection)
+      newsimh = simh(Tex.mkts[k1].collection[hk].fid,
+                     Tex.mkts[k1].collection[hk].level,
+                     Tex.mkts[k1].collection[hk].level,
+                     Dict{neighbors,hstate}(),
+                     Array{shortrec,1}(),
+                     false)
+      for hk2 in keys(Tex.mkts[k1].collection)
+        if hk2 != hk
+          push!(newsimh.ns, shortrec(Tex.mkts[k1].collection[hk2].fid,
+                                     Tex.mkts[k1].collection[hk2].lat,
+                                     Tex.mkts[k1].collection[hk2].long,
+                                     Tex.mkts[k1].collection[hk2].level,
+                                     Tex.mkts[k1].collection[hk2].level,
+                                     neighbors(0,0,0,0,0,0,0,0,0),
+                                     ChoicesAvailable(Tex.mkts[k1].collection[hk2]),
+                                     Tex.mkts[k1].collection[hk2].chprobability))
+        end
+      end
+      # NB: Here iterate over newsimh to append records of neighbors.
+      for el in newsimh.ns
+        for el2 in newsimh.ns
+          if el.fid != el2.fid
+            if (distance(el.lat, el.long, el2.lat, el2.long)>0)&(distance(el.lat, el.long, el2.lat, el2.long)<5)
+              if el2.level == 1
+                el.ns.level105+=1
+              elseif el2.level == 2
+                  el.ns.level205+=1
+              else # equals 3
+                  el.ns.level305+=1
+              end
+            elseif (distance(el.lat, el.long, el2.lat, el2.long)>=5)&(distance(el.lat, el.long, el2.lat, el2.long)<15)
+              if el2.level == 1
+                  el.ns.level1515+=1
+              elseif el2.level == 2
+                  el.ns.level2515+=1
+              else # equals 3
+                  el.ns.level3515+=1
+              end
+            elseif (distance(el.lat, el.long, el2.lat, el2.long)>=15)&(distance(el.lat, el.long, el2.lat, el2.long)<=25)
+              if el2.level == 1
+                  el.ns.level11525+=1
+              elseif el2.level == 2
+                  el.ns.level21525+=1
+              else # equals 3
+                  el.ns.level31525+=1
+              end
+            end
+          end
+        end
+      end
+      push!(outp.all, newsimh)
+    end
+  end
+  return outp
 end
 
 
