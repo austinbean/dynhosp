01b92fb6a36b52b2681071cb87cb53ca1567f356
Austin Bean <austinbean@gmail.com>
2017-02-25 12:57:39 -0600

    working with base csv

--- Reboot.jl
+++ Reboot.jl
@@ -3,6 +3,7 @@
 
 #begin
   ##  Figure out which machine I'm on
+
   dir = pwd()
   global pathdata = "";global pathpeople = "";global  pathprograms = "";
   if dir == "/Users/austinbean/Desktop/dynhosp"
@@ -23,6 +24,8 @@
     global pathprograms = "/home1/04179/abean/dynhosp/"
   end
   ###### Import the hospital data and convert to a matrix -
+    # TODO -
+
   println("Importing Hosp Data")
     dataf = DataFrames.readtable(pathdata*"TX Transition Probabilities.csv", header = true);
     for i in names(dataf)
@@ -42,96 +45,70 @@
     data = convert(Matrix, dataf);
     dataf = 0; #set to zero to clear out.
 
-      medicaidmodcoeffs = DataFrames.readtable(pathpeople*"TX 2005 Medicaid Model.csv", header = true);
-      global const medicaiddistance_c = medicaidmodcoeffs[1, 2]
-      global const medicaiddistsq_c = medicaidmodcoeffs[2, 2]
-      global const medicaidneoint_c = medicaidmodcoeffs[3, 2]
-      global const medicaidsoloint_c = medicaidmodcoeffs[4, 2]
-      global const medicaidclosest_c = medicaidmodcoeffs[5, 2]
-      global const medicaiddistbed_c = medicaidmodcoeffs[6, 2]
+
+#DONE 
+      medcoeffs, medcoeffsnames = readcsv(pathpeople*"TX 2005 Medicaid Model.csv", header = true);
+
+      medicaiddistance_c = medcoeffs[1,2] 
+      medicaiddistsq_c = medcoeffs[2,2]
+      medicaidneoint_c = medcoeffs[3,2]
+      medicaidsoloint_c = medcoeffs[4,2]
+      medicaidclosest_c = medcoeffs[5,2]
+      medicaiddistbed_c = medcoeffs[6,2]
       medicaiddemandmodelparameters = [medicaiddistance_c medicaiddistsq_c medicaidneoint_c medicaidsoloint_c medicaidclosest_c medicaiddistbed_c]
 
-      privatemodcoeffs = DataFrames.readtable(pathpeople*"TX 2005 Private Ins Model.csv", header = true);
-      global const privatedistance_c = privatemodcoeffs[1, 2]
-      global const privatedistsq_c = privatemodcoeffs[2, 2]
-      global const privateneoint_c = privatemodcoeffs[3, 2]
-      global const privatesoloint_c = privatemodcoeffs[4, 2]
-      global const privateclosest_c = privatemodcoeffs[5, 2]
-      global const privatedistbed_c = privatemodcoeffs[6, 2]
+      privcoeffs, privcoeffsnames = readcsv(pathpeople*"TX 2005 Private Ins Model.csv", header = true)
+
+      privatedistance_c = privcoeffs[1,2]
+      privatedistsq_c = privcoeffs[2,2]
+      privateneoint_c = privcoeffs[3,2]
+      privatesoloint_c = privcoeffs[4,2]
+      privateclosest_c = privcoeffs[5,2]
+      privatedistbed_c = privcoeffs[6,2]
       privatedemandmodelparameters = [privatedistance_c privatedistsq_c privateneoint_c privatesoloint_c privateclosest_c privatedistbed_c]
 
+
+
+#DONE 
     println("Importing Medicaid Patients") # use the infants only.
-    medicaid = DataFrames.readtable(pathpeople*"TX 2005 Medicaid Individual Choices.csv", header = true);
-    #people = DataFrames.readtable(pathpeople*"TX 2005 1 Individual Choices.csv", header = true); #smaller version for testing.
-    for i in names(medicaid)
-      if ( typeof(medicaid[i]) == DataArrays.DataArray{Float64,1} )
-        medicaid[DataFrames.isna(medicaid[i]), i] = 0
-      elseif (typeof(medicaid[i]) == DataArrays.DataArray{Int64,1})
-        medicaid[DataFrames.isna(medicaid[i]), i] = 0
-      elseif typeof(medicaid[i]) == DataArrays.DataArray{String,1}
-        # A dumb way to make sure no one chooses a missing facility: set covariate values to large numbers
-        # with opposite signs of the corresponding coefficients from modelparameters.
-        # This does that by looking at missing NAMES, not fids.
-        medicaid[DataFrames.isna(medicaid[i]), medicaid.colindex.lookup[i]+2] = -sign(medicaidneoint_c)*99
-        medicaid[DataFrames.isna(medicaid[i]), medicaid.colindex.lookup[i]+8] = -sign(medicaidsoloint_c)*99
-        medicaid[DataFrames.isna(medicaid[i]), i] = "NONE"
-      elseif typeof(medicaid[i]) == DataArrays.DataArray{String,1}
-        medicaid[DataFrames.isna(medicaid[i]), medicaid.colindex.lookup[i]+2] = -sign(medicaidneoint_c)*99
-        medicaid[DataFrames.isna(medicaid[i]), medicaid.colindex.lookup[i]+8] = -sign(medicaidsoloint_c)*99
-        medicaid[DataFrames.isna(medicaid[i]), i] = "NONE"
-      end
-      if sum(size(medicaid[DataFrames.isna(medicaid[i]), i]))>0
-        println(i)
-      end
-    end
-    pmedicaid = convert(Matrix, medicaid);
-    medicaid = 0; # DataFrame not used - set to 0 and clear out.
-    for i =1:size(pmedicaid, 2)
-      if (typeof(pmedicaid[2,i])==String) | (typeof(pmedicaid[2,i])==String)
-        #      print(i, "\n")
-        pmedicaid[:,i] = "0"
-        pmedicaid[:,i] = map(x->parse(Float64, x), pmedicaid[:,i])
-      end
-    end
+    pmedicaid, medicaidnames = readcsv("/Users/austinbean/Google Drive/Texas Inpatient Discharge/TX 2005 Medicaid Individual Choices.csv", header = true)
+
+    for i = 1:size(pinsured,1)
+        for j = 1:size(pinsured,2)
+            if typeof(pinsured[i,j])<:AbstractString
+                if (pinsured[i,j]=="")
+                    pinsured[i,j] = 0 # = 0
+                end 
+            end
+        end 
+    end 
+
     # Note this change - I don't think there's anything that requires 64 bits.
     pmedicaid = convert(Array{Float32, 2}, pmedicaid)
   #  println("Size of Medicaid, ", size(pmedicaid))
 
+
+#DONE 
     println("Importing Privately Insured Patients") #use the infants only.
-    pinsure = DataFrames.readtable(pathpeople*"TX 2005 Private Ins Individual Choices.csv", header = true);
-    for i in names(pinsure)
-      if ( typeof(pinsure[i]) == DataArrays.DataArray{Float64,1} )
-        pinsure[DataFrames.isna(pinsure[i]), i] = 0
-      elseif (typeof(pinsure[i]) == DataArrays.DataArray{Int64,1})
-        pinsure[DataFrames.isna(pinsure[i]), i] = 0
-      elseif typeof(pinsure[i]) == DataArrays.DataArray{String,1}
-        # A dumb way to make sure no one chooses a missing facility: set covariate values to large numbers
-        # with opposite signs of the corresponding coefficients from modelparameters.
-        # This does that by looking at missing NAMES, not fids.
-        pinsure[DataFrames.isna(pinsure[i]), pinsure.colindex.lookup[i]+2] = -sign(privateneoint_c)*99
-        pinsure[DataFrames.isna(pinsure[i]), pinsure.colindex.lookup[i]+8] = -sign(privatesoloint_c)*99
-        pinsure[DataFrames.isna(pinsure[i]), i] = "NONE"
-      elseif typeof(pinsure[i]) == DataArrays.DataArray{String,1}
-        pinsure[DataFrames.isna(pinsure[i]), pinsure.colindex.lookup[i]+2] = -sign(privateneoint_c)*99
-        pinsure[DataFrames.isna(pinsure[i]), pinsure.colindex.lookup[i]+8] = -sign(privatesoloint_c)*99
-        pinsure[DataFrames.isna(pinsure[i]), i] = "NONE"
-      end
-      if sum(size(pinsure[DataFrames.isna(pinsure[i]), i]))>0
-        println(i)
-      end
-    end
-     pinsured = convert(Matrix,pinsure);
-     pinsure= 0; # DataFrame not used - set to 0 and clear out.
-    for i =1:size(pinsured, 2)
-      if (typeof(pinsured[2,i])==String) | (typeof(pinsured[2,i])==String)
-  #      print(i, "\n")
-        pinsured[:,i] = "0"
-        pinsured[:,i] = map(x->parse(Float64, x), pinsured[:,i])
-      end
-    end
+    
+    pinsured, pinsurednames = readcsv("/Users/austinbean/Google Drive/Texas Inpatient Discharge/TX 2005 Private Ins Individual Choices.csv", header = true)
+
+
+    for i = 1:size(pinsured,1)
+        for j = 1:size(pinsured,2)
+            if typeof(pinsured[i,j])<:AbstractString
+                if (pinsured[i,j]=="")
+                    pinsured[i,j] = 0 # = 0
+                end 
+            end
+        end 
+    end 
     # Note this change - I don't think there's anything that requires 64 bits.
      pinsured= convert(Array{Float32, 2}, pinsured)
   #   println("Size of Privately Insured, ", size(pinsured))
+
+
+  # TODO 
      # TX zip codes:
      TXzps = DataFrames.readtable(pathprograms*"TXzipsonly.csv", header = false);
      TXzips = convert(Vector, TXzps[:x1]);
@@ -147,11 +124,7 @@
     txfd = DataFrames.readtable(pathprograms*"TXfidsonly.csv", header = true)
     allfids = convert(Vector, txfd[:fid])
 
-    # WTP values -
-    # txwp = DataFrames.readtable(pathprograms*"WTPTemplate.csv", header = true)
-    # WTP = convert(Matrix, txwp)
-  #  global const nsims = 500;
-  #  global const dirs = pwd() # present working directory path
+
   zips = DataFrames.readtable(pathprograms*"TXzipsonly.csv", header = false);
   zips = convert(Array, zips[:,1]);
   choices = DataFrames.readtable(pathdata*"TX Zip Code Choice Sets.csv", header = true);
