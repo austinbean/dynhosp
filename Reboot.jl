d798301f4e2d063bdd9327063326fd2112703952
Austin Bean <austinbean@gmail.com>
2017-02-27 11:56:55 -0600

    path for work added

--- Reboot.jl
+++ Reboot.jl
@@ -22,6 +22,10 @@
     global pathdata = "/home1/04179/abean/dynhosp/"
     global pathpeople = "/home1/04179/abean/dynhosp/"
     global pathprograms = "/home1/04179/abean/dynhosp/"
+  elseif (dir=="/work/04179/abean/dynhosp")
+    global pathdata = "/work/04179/abean/dynhosp"
+    global pathpeople = "/work/04179/abean/dynhosp"
+    global pathprogram = "/work/04179/abean/dynhosp"
   end
   ###### Import the hospital data and convert to a matrix -
 
