37276b7ce7c6e02656f66b538c12c99cde34ffc2
Austin Bean <austinbean@gmail.com>
2017-04-26 20:54:04 -0500

    fix in ExactChoice needed at line 1423

--- Counter2.jl
+++ Counter2.jl
@@ -1230,7 +1230,7 @@ PatientZero(p1, p2)
 
 NOTES on current problems:
 - 04/25: what I need is something like: full update and partial update.  
-- some firms  
+- some firms  are getting added... that is, fids are getting added which I don't want added.  Where does that happen?  
 
 """
 function ExactVal(D::DynState,
@@ -1251,6 +1251,9 @@ function ExactVal(D::DynState,
   its::Int64 = 0                                                                      # records iterations, but will be dropped after debugging.
   for el in chunk
     neighbors::Array{Int64,1} = FindComps(D.all[el], D)                               #  these are addresses of fids.
+    for el in neighbors
+      println(D.all[el].fid) # this should find 1.
+    end 
     outvals[D.all[el].fid] = Dict{NTuple{10, Int64}, Float64 }()
     tempvals[D.all[el].fid] = Dict{NTuple{10, Int64}, Float64 }()
     stdict = StateRecord(D.all[el].nfids, el, D)                                      # returns the restricted state.
@@ -1299,6 +1302,172 @@ function ExactVal(D::DynState,
   # Return equilibrium values:
   return outvals
 end
+
+
+
+
+
+"""
+`ExactChoice(temp::Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }, 
+                     stable::Dict{ Int64, Dict{NTuple{10, Int64},  Float64 } }, 
+                     fid::Int64, 
+                     location::Int64,
+                     p1::patientcount,
+                     p2::patientcount,
+                     competitors::Array{Int64,1},
+                     D::DynState; )`
+What action should the firm choose?
+Takes two dictionaries, the DynState, computes the best action, returns the value of the action.
+Needs to: 
+- compute the demand in expectation at EACH possible level.
+- compute the profit at EACH possible level.
+- state will be recorded in the dyn record. 
+- But the key thing is: return the VALUE of the state.   
+NB - level won't change.  I can compute the value of being in all of these states depending on the level.
+ Ok - the thing is that this must be done for Both facilities and their neighbors, but the notion of the state 
+ for neighbors is different. This is important. 
+
+
+##### TESTING ######
+TexasEq = CreateEmpty(ProjectModule.fips, ProjectModule.alldists, 50);
+Tex = EntireState(Array{Market,1}(), Dict{Int64, Market}(), Dict{Int64, Int64}());
+CMakeIt(Tex, ProjectModule.fips);
+FillState(Tex, ProjectModule.alldists, 50);
+patients = NewPatients(Tex);
+
+dyn = DynStateCreate(TexasEq, Tex, patients); 
+
+# To Run:
+
+d1 = Dict{ Int64, Dict{NTuple{10, Int64}, Float64}  }()
+d2 = Dict{ Int64, Dict{NTuple{10, Int64}, Float64}  }()
+p1 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+p2 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+#fid = 3490795;
+#location = 1;
+
+d1[dyn.all[1].fid] = Dict{NTuple{10,Int64}, Float64}()
+d1[dyn.all[1].fid][StateKey(dyn.all[1], dyn.all[1].level)] = 0.0
+d1[dyn.all[1].fid][StateKey(dyn.all[1], 2)] = 0.0
+d1[dyn.all[1].fid][StateKey(dyn.all[1], 3)] = 0.0
+
+
+ExactChoice(d1, d2, dyn.all[1].fid, 1, p1, p2,  dyn)
+d1[dyn.all[1].fid]
+
+
+EXTRA: 
+
+                # FIXME - here is a problem.  Keys are being added to these dicts inconsistently.  Don't add all of them.  Why 
+                # are these needed anyway? 
+                # I want to *not* add these.  What will that break?   
+                # if !haskey(stable, fid) # this should not be necessary when this is debugged.  
+                #   stable[fid] = Dict{NTuple{10, Int64},  Float64 }()
+                # end 
+                # for el in keys(recs) # this adds a record for each of the (state,level) options.  They are put in the stable dict.  
+                #   if !haskey(stable, el)
+                #     stable[el] = Dict{NTuple{10,Int64}, Float64}()
+                #   end
+                #   if !haskey( stable[el], TAddLevel(recs[el], 1) )
+                #     stable[el][TAddLevel(recs[el], 1)] = 0.5
+                #   end 
+                #   if !haskey( stable[el], TAddLevel(recs[el], 2) )
+                #     stable[el][TAddLevel(recs[el], 2)] = 0.5
+                #   end 
+                #   if !haskey( stable[el], TAddLevel(recs[el], 3) )
+                #     stable[el][TAddLevel(recs[el], 3)] = 0.5
+                #   end 
+                # end 
+
+    # FIXME - I don't want to FindComps when this is a neighbor.
+    # should I permanently take as arguments all of the neighbors I want to do?  
+    # think about the 2-3 firm case... especially with non-overlapping sets of neighbors.  
+"""
+
+function ExactChoice(temp::Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }, 
+                     stable::Dict{ Int64, Dict{NTuple{10, Int64},  Float64 } }, 
+                     nbs::Dict{Int64, Bool},
+                     fid::Int64, 
+                     location::Int64,
+                     p1::patientcount,
+                     p2::patientcount,
+                     D::DynState; 
+                     messages::Bool = true, 
+                     β::Float64 = 0.95,
+                     ϕ13::Float64 = 0.0, # substitute values and scale these!
+                     ϕ12::Float64 = 0.0,
+                     ϕ1EX::Float64 = 0.0,
+                     ϕ23::Float64 = 0.0,
+                     ϕ21::Float64 = 0.0,
+                     ϕ2EX::Float64 = 0.0,
+                     ϕ31::Float64 = 0.0,
+                     ϕ32::Float64 = 0.0,
+                     ϕ3EX::Float64 = 0.0)  
+
+    neighbors::Array{Int64,1} = FindComps(D.all[location], D) # find the competitors.  
+    # FIXME 04/25/2017 - the call to ContProbs already fs up because its looking for something that isn't there. 
+    # but it isn't looking for it in stable.  it's in nlocs. 
+    println("Exact Choice called on: ", fid)
+    nloc = Array{Int64,1}() # initialize empty.
+    if nbs[D.all[location].fid] # this will be true when this is a "neighbor" only, not a real facility.  
+      nloc = FindComps(D.all[location], D) 
+    else
+      nloc = Array{Int64,1}() # I guess this array will be empty when the firm is the neighbor.  
+    end 
+    # FIXME - there are two calls to this function.  I don't seem to use "recs"
+    recs = StateRecord(neighbors, location, D) # this returns the dict of the state for the firm whose value is being computed from point of view of the main firm.
+    rec = StateRecord(D.all[location].nfids, location, D)     # this is computing the state from the point of view of... the main fac. 
+    println("From Exact Choice ") 
+    println("keys of stable: ", keys(stable)) 
+    println(" temp keys: ",keys(temp))
+# 04/26/2017 FIXME - the extra fid is here.  This searches in ContProbs USING rec.  
+    println("state record ", rec)   
+    println("nlocs: ", nloc)
+    for el in nloc 
+      println("fid is: ", D.all[el].fid)
+    end 
+    println("the fid: ", fid)
+    println("set of neighbors: ", neighbors)
+    println("Exact Choice nbs: ", nbs)
+    cps::Dict{Int64,Array{Float64,1}} = ContProbs(rec, nloc, stable, D)  
+    nstates::Dict{NTuple{9,Int64},Float64} = TotalCombine(D, location, D.all[location].nfids, cps)
+    CV1::Float64 = ContVal(nstates, fid, stable ,1)
+    CV2::Float64 = ContVal(nstates, fid, stable ,2)
+    CV3::Float64 = ContVal(nstates, fid, stable ,3)   
+    
+    if messages 
+      
+      println("stable keys before", keys(stable))  
+      println("CV's: ", CV1, " ", CV2, " ", CV3) 
+      println("the max was ", temp[fid][StateKey(D.all[location],1)]) 
+      println("the rev was ", PatientRev(D.all[location],p1,p2,10)) 
+    end
+  # Update value at Level 1
+    D.all[location].level = 1
+    UpdateD(D.all[location])                                  # updates the utility for a new level 
+    DSimNew( D.all[location].mk, fid, p1, p2)                 # Computes the demand for that level. 
+    temp[fid][StateKey(D.all[location],1)] = maximum([ϕ1EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([β*(CV1),-ϕ12+β*(CV2),-ϕ13+β*(CV3)])])
+    D.all[location].level = D.all[location].actual            # resets the level 
+    UtilDown(D.all[location])                                 # resets the utility
+    PatientZero(p1, p2)                                       # overwrites the patientcount with zeros 
+  # Update value at Level 2 (repeats steps above!)
+    D.all[location].level = 2
+    UpdateD(D.all[location]) # Updates deterministic part of utility.  
+    DSimNew( D.all[location].mk, fid, p1, p2) # Computes the demand.   
+    temp[fid][StateKey(D.all[location],2)] = maximum([ϕ2EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([-ϕ21+β*(CV1),β*(CV2),-ϕ23+β*(CV3)])])
+    D.all[location].level = D.all[location].actual
+    UtilDown(D.all[location])
+    PatientZero(p1, p2)
+  # Update value at Level 3
+    D.all[location].level = 3
+    UpdateD(D.all[location])
+    DSimNew( D.all[location].mk, fid, p1, p2) # Computes the demand.
+    temp[fid][StateKey(D.all[location],3)] = maximum([ϕ3EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([-ϕ31+β*(CV1),-ϕ32+β*(CV2),β*(CV3)])])
+    D.all[location].level = D.all[location].actual
+    UtilDown(D.all[location])
+    PatientZero(p1, p2)
+end 
+
 
 
 
@@ -1377,165 +1546,6 @@ function ExactConvergence(current::Dict{ Int64, Dict{NTuple{10, Int64}, Float64 
   return converge, newchunk
 end 
 
-
-
-
-
-"""
-`ExactChoice(temp::Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }, 
-                     stable::Dict{ Int64, Dict{NTuple{10, Int64},  Float64 } }, 
-                     fid::Int64, 
-                     location::Int64,
-                     p1::patientcount,
-                     p2::patientcount,
-                     competitors::Array{Int64,1},
-                     D::DynState; )`
-What action should the firm choose?
-Takes two dictionaries, the DynState, computes the best action, returns the value of the action.
-Needs to: 
-- compute the demand in expectation at EACH possible level.
-- compute the profit at EACH possible level.
-- state will be recorded in the dyn record. 
-- But the key thing is: return the VALUE of the state.   
-NB - level won't change.  I can compute the value of being in all of these states depending on the level.
- Ok - the thing is that this must be done for Both facilities and their neighbors, but the notion of the state 
- for neighbors is different. This is important. 
-
-
-##### TESTING ######
-TexasEq = CreateEmpty(ProjectModule.fips, ProjectModule.alldists, 50);
-Tex = EntireState(Array{Market,1}(), Dict{Int64, Market}(), Dict{Int64, Int64}());
-CMakeIt(Tex, ProjectModule.fips);
-FillState(Tex, ProjectModule.alldists, 50);
-patients = NewPatients(Tex);
-
-dyn = DynStateCreate(TexasEq, Tex, patients); 
-
-# To Run:
-
-d1 = Dict{ Int64, Dict{NTuple{10, Int64}, Float64}  }()
-d2 = Dict{ Int64, Dict{NTuple{10, Int64}, Float64}  }()
-p1 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-p2 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-#fid = 3490795;
-#location = 1;
-
-d1[dyn.all[1].fid] = Dict{NTuple{10,Int64}, Float64}()
-d1[dyn.all[1].fid][StateKey(dyn.all[1], dyn.all[1].level)] = 0.0
-d1[dyn.all[1].fid][StateKey(dyn.all[1], 2)] = 0.0
-d1[dyn.all[1].fid][StateKey(dyn.all[1], 3)] = 0.0
-
-
-ExactChoice(d1, d2, dyn.all[1].fid, 1, p1, p2,  dyn)
-d1[dyn.all[1].fid]
-
-
-EXTRA: 
-
-                # FIXME - here is a problem.  Keys are being added to these dicts inconsistently.  Don't add all of them.  Why 
-                # are these needed anyway? 
-                # I want to *not* add these.  What will that break?   
-                # if !haskey(stable, fid) # this should not be necessary when this is debugged.  
-                #   stable[fid] = Dict{NTuple{10, Int64},  Float64 }()
-                # end 
-                # for el in keys(recs) # this adds a record for each of the (state,level) options.  They are put in the stable dict.  
-                #   if !haskey(stable, el)
-                #     stable[el] = Dict{NTuple{10,Int64}, Float64}()
-                #   end
-                #   if !haskey( stable[el], TAddLevel(recs[el], 1) )
-                #     stable[el][TAddLevel(recs[el], 1)] = 0.5
-                #   end 
-                #   if !haskey( stable[el], TAddLevel(recs[el], 2) )
-                #     stable[el][TAddLevel(recs[el], 2)] = 0.5
-                #   end 
-                #   if !haskey( stable[el], TAddLevel(recs[el], 3) )
-                #     stable[el][TAddLevel(recs[el], 3)] = 0.5
-                #   end 
-                # end 
-
-"""
-
-function ExactChoice(temp::Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }, 
-                     stable::Dict{ Int64, Dict{NTuple{10, Int64},  Float64 } }, 
-                     nbs::Dict{Int64, Bool},
-                     fid::Int64, 
-                     location::Int64,
-                     p1::patientcount,
-                     p2::patientcount,
-                     D::DynState; 
-                     messages::Bool = true, 
-                     β::Float64 = 0.95,
-                     ϕ13::Float64 = 0.0, # substitute values and scale these!
-                     ϕ12::Float64 = 0.0,
-                     ϕ1EX::Float64 = 0.0,
-                     ϕ23::Float64 = 0.0,
-                     ϕ21::Float64 = 0.0,
-                     ϕ2EX::Float64 = 0.0,
-                     ϕ31::Float64 = 0.0,
-                     ϕ32::Float64 = 0.0,
-                     ϕ3EX::Float64 = 0.0)  
-    # FIXME - I don't want to FindComps when this is a neighbor.
-    # should I permanently take as arguments all of the neighbors I want to do?  
-    # think about the 2-3 firm case... especially with non-overlapping sets of neighbors.  
-    neighbors::Array{Int64,1} = FindComps(D.all[location], D) # find the competitors.  
-    recs = StateRecord(neighbors, location, D) # this returns the dict of the state for the firm whose value is being computed from point of view of the main firm.
-    nloc = Array{Int64,1}() # initialize empty.
-    if nbs[D.all[location].fid] # this will be true when this is a "neighbor" only, not a real facility.  
-      nloc = FindComps(D.all[location], D) 
-    else
-      nloc = Array{Int64,1}() # I guess this array will be empty when the firm is the neighbor.  
-    end 
-    # FIXME - think about the next line: finds the competitors' locations for the main firm.  But what about the neighbors?
-    # it will find them too. But I don't want to look for them.  I do need to compute the CV, but I do not want to do this
-    # for the "neighbors of the neighbors".  I want to identify and separately handle these firms.  
-    # here fix the fact that only the neighbors of the simulated firm need to be looked up.
-    rec = StateRecord(D.all[location].nfids, location, D)     # this is computing the state from the point of view of... the main fac.  
-    # FIXME 04/25/2017 - the call to ContProbs already fs up because its looking for something that isn't there. 
-    println("state record ", rec)
-    println("nlocs: ", nloc)
-    cps::Dict{Int64,Array{Float64,1}} = ContProbs(rec, nloc, stable, D)  
-    nstates::Dict{NTuple{9,Int64},Float64} = TotalCombine(D, location, D.all[location].nfids, cps)
-    CV1::Float64 = ContVal(nstates, fid, stable ,1)
-    CV2::Float64 = ContVal(nstates, fid, stable ,2)
-    CV3::Float64 = ContVal(nstates, fid, stable ,3)   
-    temp[fid][StateKey(D.all[location],1)] = maximum([ϕ1EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([β*(CV1),-ϕ12+β*(CV2),-ϕ13+β*(CV3)])])
-    
-    if messages 
-      println("From Exact Choice ") 
-      println("the fid: ", fid)
-      println("set of neighbors: ", neighbors)
-      println("Exact Choice nbs: ", nbs)
-      println(" temp keys before",keys(temp))  
-      println("stable keys before", keys(stable))  
-      println("CV's: ", CV1, " ", CV2, " ", CV3) 
-      println("the max was ", temp[fid][StateKey(D.all[location],1)]) 
-      println("the rev was ", PatientRev(D.all[location],p1,p2,10)) 
-    end
-
-  # Update value at Level 1
-    D.all[location].level = 1
-    UpdateD(D.all[location])                                  # updates the utility for a new level 
-    DSimNew( D.all[location].mk, fid, p1, p2)                 # Computes the demand for that level. 
-    D.all[location].level = D.all[location].actual            # resets the level 
-    UtilDown(D.all[location])                                 # resets the utility
-    PatientZero(p1, p2)                                       # overwrites the patientcount with zeros 
-  # Update value at Level 2 (repeats steps above!)
-    D.all[location].level = 2
-    UpdateD(D.all[location]) # Updates deterministic part of utility.  
-    DSimNew( D.all[location].mk, fid, p1, p2) # Computes the demand.   
-    temp[fid][StateKey(D.all[location],2)] = maximum([ϕ2EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([-ϕ21+β*(CV1),β*(CV2),-ϕ23+β*(CV3)])])
-    D.all[location].level = D.all[location].actual
-    UtilDown(D.all[location])
-    PatientZero(p1, p2)
-  # Update value at Level 3
-    D.all[location].level = 3
-    UpdateD(D.all[location])
-    DSimNew( D.all[location].mk, fid, p1, p2) # Computes the demand.
-    temp[fid][StateKey(D.all[location],3)] = maximum([ϕ3EX, PatientRev(D.all[location],p1,p2,10)+β*maximum([-ϕ31+β*(CV1),-ϕ32+β*(CV2),β*(CV3)])])
-    D.all[location].level = D.all[location].actual
-    UtilDown(D.all[location])
-    PatientZero(p1, p2)
-end 
 
 
 
@@ -1699,6 +1709,7 @@ d1[dyn.all[152].fid][TAddLevel(recs2[dyn.all[152].fid], 3)] = 0.0
 cp = ContProbs(recs2, location2, d1, dyn)
 
 # should return: Dict{Int64,Array{Float64,1}} with 2 entries:  672285 => [0.333333, 0.333333, 0.333333] 373510 => [0.333333, 0.333333, 0.333333]
+# FIXME - this is a problem too.  It's iterating over all of these keys.  Why?  OR - what keys and why do I want them?
 
 """
 function ContProbs(state_recs::Dict{Int64,NTuple{9,Int64}},
