fa737ac6b1f7e9e3d53bcb945bd9547b5ea21a87
Austin Bean <austinbean@gmail.com>
2017-05-24 11:46:57 -0500

    updating deterministic, but level is changing too much

--- ValApprox.jl
+++ ValApprox.jl
@@ -25,38 +25,43 @@ function ValApprox(D::DynState, V::allvisits, itlim::Int64; chunk::Array{Int64,1
   for el in chunk                                                          # creates a dictionary of visited records.
     V.all[D.all[el].fid] = vrecord( Array{Tuple{Int64,Int64,Int64,Int64,Int64,Int64,Int64,Int64,Int64,Int64,Int64}, 1}(), 1)
   end
+  upcount = 0
   while (iterations<itlim)&&(!converged)
     for el in D.all[chunk]
       if !el.converged                                                     # only keep simulating with the ones which haven't converged
         DSimNew(el.mk, el.fid, a, b)
         GetProb(el)                                                        # this chooses the action by the other firms
-        # TODO: add a market size check here to do something more like oblivious in large markets.
         if !haskey(el.visited, KeyCreate(el.cns, el.level))
           println("adding new counter")
           el.visited[KeyCreate(el.cns, el.level)]=nlrec(MD(ChoicesAvailable(el), StartingVals(el, a, b)), vcat(ChoicesAvailable(el),transpose(PolicyUpdate(StartingVals(el, a, b)))), Dict(k => 0 for k in ChoicesAvailable(el)) )
         end
         Action = ChooseAction(el)                                              # Takes an action and returns it.
+        println("Action is: ", Action )
         ComputeR(el, a, b, Action, iterations; debug = debug)                  # Computes the return to the action
-        # FIXME - where is the output of the above line written?  To V?  No.  Within the Dyn record somehwere?
         level::Int64 = LevelFunction(el, Action)                               # Level may change with action, but for next period.
         if iterations <= 1_000_000
           push!(V.all[el.fid].visited, RTuple(el, Action))                     # Record the first million state-action pairs in a vector
         elseif iterations >1_000_000
           V.all[el.fid].visited[iterations%1_000_000] = RTuple(el, Action)     # Once this is a million entries long, start overwriting to keep track of only 1_000_000
         end
-        el.previous = el.level                                              # Reassign current level to previous.
-        el.level = level                                                    # Reassign current level, if it has changed or not.
-        ExCheck(el)                                                         # Checks for exit
-        FixNN(el)                                                           # Fixes the firms neighbors.
-        iterations += 1                                                     # Update iteration count - TODO: delete after debugging.
-        V.all[el.fid].totalcnt += 1                                         # Update the iteration count within the visit records.
-        PatientZero(a,b) # resets both patientcounts to zero.
+        if level != el.level                                                   # levels don't agree - i.e., "level" here is the next level which has been drawn.  
+          println("levels (next, current) ", level, "  ", el.level )
+          upcount += 1 # FIXME - always entering this loop?
+          UpdateDUtil(el)                                                      # this should update the utility for hospitals which changed level.  
+        end 
+        el.previous = el.level                                                 # Reassign current level to previous.
+        el.level = level                                                       # Reassign current level, if it has changed or not.
+        ExCheck(el)                                                            # Checks for exit
+        FixNN(el)                                                              # Fixes the firms neighbors.
+        iterations += 1                                                        # Update iteration count - TODO: delete after debugging.
+        V.all[el.fid].totalcnt += 1                                            # Update the iteration count within the visit records.
+        PatientZero(a,b)                                                       # resets both patientcounts to zero.
       end
-      # TODO - uncomment convergence test when that is debugged.
-      if iterations%1_000_0 == 0                                        # Check for convergence every million iterations
-        CheckConvergence(el, V.all[el.fid].visited) # FIXME - this is the wrong signature for this function.
+      if iterations%1_00 == 0                                                  # Check for convergence every million iterations
+        CheckConvergence(el, V.all[el.fid].visited; debug = true) 
       end
     end
   end 
+  println(upcount)
   converged = Halt(D, chunk)                                                # Check to see if all firms in "chunk" have converged, then halt if they have.
 end
