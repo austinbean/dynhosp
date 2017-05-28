f437f8b568ba78745c1ff902dbd609a5089b513f
Austin Bean <austinbean@gmail.com>
2017-05-28 17:17:07 -0500

    almost done w/ PolicyUpdate fix

--- Counter2.jl
+++ Counter2.jl
@@ -54,6 +54,9 @@ patients = NewPatients(Tex);
 dm, dp = PRanges(ProjectModule.pcount)
 h1 = simh(4530190, 30.289991, -97.726196, 3, 3, 3, 100, neighbors(0,0,0,0,0,0,0,0,0), Array{Int64,1}(), Dict{Tuple{Int64}, ProjectModule.nlrec}(), Array{ProjectModule.shortrec,1}(), DynPatients(patients, 4530190, dm, dp), false, false, false)
 
+Now use this to continue debugging.  Next is... 
+
+
 Note that the function takes TWO EntireState arguments.  This is super dumb, but
 only one of them (containing hospital types) has the bed counts.
 """
@@ -171,6 +174,7 @@ function DynStateCreate( Tex::EntireState, Tex2::EntireState, p::patientcollecti
     #FIXME - here the problem is that PolicyUpdate can't be called on StartingVals, which returns Array{Float64,1}, but changing that doesn't work because there is at least one 
     # other function which calls it.  Ok - what to do... Also this whole thing is poorly done.  
     # I think MD is the function which fails if the return type of PolicyUpdate is changed.
+    # All I really want to do is create [ Choicesavailable(el) ; PolicyUpdate(StartingValues...)]  This only requires a new Version of PolicyUpdate.  Or just pick some numbers.  
     # this is a dumb problem.  
     el.visited[KeyCreate(el.cns, el.level)] = nlrec(MD(ChoicesAvailable(el), StartingVals(el, ProjectModule.patientcount(5,6,4,13,8,41,248), ProjectModule.patientcount(5,6,4,13,8,41,248))),
                                                     vcat(ChoicesAvailable(el),transpose(PolicyUpdate(StartingVals(el, ProjectModule.patientcount(5,6,4,13,8,41,248), ProjectModule.patientcount(5,6,4,13,8,41,248)))  )),
@@ -1126,7 +1130,7 @@ p1 = patientcount(100,100,100,100,100,100,100)
 p2 = patientcount(100,100,100,100,100,100,100)
 
 
-SinglePay(h1, p1, p2, 10)
+SinglePay(h1, p1, p2, 10) == 0.073280454324209 # returns true. 
 
 """
 function SinglePay(s::simh,
