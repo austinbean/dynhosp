392c4eeb4b9f6b46d529bfc820b763d2887723bb
Austin Bean <austinbean@gmail.com>
2017-06-11 11:16:34 -0500

    added constant designations

--- LogitEst.jl
+++ LogitEst.jl
@@ -13,8 +13,8 @@ function states1(lev1::Int64, lev2::Int64, lev3::Int64)
     # st12 =  [0 1 max(lev1-1, 0) lev2+1 lev3]
     # st13 =  [1 0 max(lev1-1,0) lev2 lev3+1]
     # st1ex = [0 0 max(lev1-1,0) lev2 lev3]
-  Result = [0 0 lev1 lev2 lev3; 0 1 max(lev1-1, 0) lev2+1 lev3; 1 0 max(lev1-1,0) lev2 lev3+1; 0 0 max(lev1-1,0) lev2 lev3]
-  return Result
+  #Result = [0 0 lev1 lev2 lev3; 0 1 max(lev1-1, 0) lev2+1 lev3; 1 0 max(lev1-1,0) lev2 lev3+1; 0 0 max(lev1-1,0) lev2 lev3]
+  return [0 0 lev1 lev2 lev3; 0 1 max(lev1-1, 0) lev2+1 lev3; 1 0 max(lev1-1,0) lev2 lev3+1; 0 0 max(lev1-1,0) lev2 lev3] #Result
 end
 
 
@@ -27,8 +27,8 @@ function states2(lev1::Int64, lev2::Int64, lev3::Int64)
     # st22  = [0 1 lev1 lev2 lev3]
     # st23  = [1 0 lev1 max(lev2-1, 0) lev3]
     # st2ex = [0 0 lev1 max(lev2-1, 0) lev3]
-  Result = [0 0 lev1+1 max(lev2-1, 0) lev3; 0 1 lev1 lev2 lev3; 1 0 lev1 max(lev2-1, 0) lev3; 0 0 lev1 max(lev2-1, 0) lev3]
-  return Result
+  #Result = [0 0 lev1+1 max(lev2-1, 0) lev3; 0 1 lev1 lev2 lev3; 1 0 lev1 max(lev2-1, 0) lev3; 0 0 lev1 max(lev2-1, 0) lev3]
+  return [0 0 lev1+1 max(lev2-1, 0) lev3; 0 1 lev1 lev2 lev3; 1 0 lev1 max(lev2-1, 0) lev3; 0 0 lev1 max(lev2-1, 0) lev3] #Result
 end
 
 
@@ -41,8 +41,8 @@ function states3(lev1::Int64, lev2::Int64, lev3::Int64)
     # st32  = [0 1 lev1 lev2+1 max(lev3-1,0)]
     # st33  = [1 0 lev1 lev2 lev3]
     # st3ex = [0 0 lev1 lev2 max(lev3-1,0)]
-  Result = [0 0 lev1+1 lev2 max(lev3-1,0); 0 1 lev1 lev2+1 max(lev3-1,0); 1 0 lev1 lev2 lev3; 0 0 lev1 lev2 max(lev3-1,0)]
-  return Result
+  #Result = [0 0 lev1+1 lev2 max(lev3-1,0); 0 1 lev1 lev2+1 max(lev3-1,0); 1 0 lev1 lev2 lev3; 0 0 lev1 lev2 max(lev3-1,0)]
+  return [0 0 lev1+1 lev2 max(lev3-1,0); 0 1 lev1 lev2+1 max(lev3-1,0); 1 0 lev1 lev2 lev3; 0 0 lev1 lev2 max(lev3-1,0)] #Result
 end
 
 
@@ -61,15 +61,15 @@ function NewPoly(numlev::Int64, level::Int64)  # first arg is number of facs at 
   coeffs1::Array{Float64,2} = [-0.344434  0.0463555  -0.0371076  0.0159125  -0.00313691  0.000333805  -1.99556e-5  6.31e-7  -8.22e-9] #ProjectModule.Poly1
   outp::Float64 = 0.0
   if level == 1
-    for i = 1:maximum(size(coeffs1))
+    for i = 1:9 #size(coeffs1,2)
       outp+=coeffs1[i]*numlev^(i+1)
     end 
   elseif level == 2
-    for i = 1:maximum(size(coeffs2))
+    for i = 1:8 #size(coeffs2,2)
       outp+=coeffs2[i]*numlev^(i+1)
     end 
   elseif level == 3
-    for i = 1:maximum(size(coeffs3))
+    for i = 1:9 # size(coeffs3,2)
       outp+=coeffs3[i]*numlev^(i+1)
     end     
   end 
@@ -88,7 +88,7 @@ end
 
 function NeighborsProd(cases::Array{Float64,2}, neighbors::Array{Int64})
   outp::Float64 = 0.0
-  for i = 1:maximum(size(cases))
+  for i = 1:size(cases,2)
     outp += cases[i]*neighbors[i]
   end 
   return outp
@@ -96,93 +96,110 @@ end
 
 
 
+"""
+`logitest(ownlev::Tuple, lev1::Int64, lev2::Int64, lev3::Int64, neighbors::Array)`
+
+Duplicates the first stage logit estimates function, originally estimated in Stata.  
+
+### Test ###
+
+isapprox(logitest((1,0), 2, 3, 4, [0,0,0,0,0,0,0,0,0]),[0.011972779588994365  0.9590086489509424  0.028853245233598165  0.00016532622646518638]) # returns true 
+logitest((1,0), 2, 3, 4, [0,0,0,0,0,0,0,0,0]) == [0.011972779588994365  0.9590086489509424  0.028853245233598165  0.00016532622646518638] # returns true 
+
+for i = 1:10
+  @time logitest((1,0), 2, 3, 4, [0,0,0,0,0,0,0,0,0])
+end 
+
+
+"""
 function logitest(ownlev::Tuple, lev1::Int64, lev2::Int64, lev3::Int64, neighbors::Array )
   push!(neighbors,1) # adds a 1 at the end of the neighbors to handle the choice-specific constant.
-  # here is where to insert the constants... NeighborsProd uses caseXY and neighbors  
   # Casevars 13
-  lev105_13::Float64 = ProjectModule.regcoeffs[32, 2]
-  lev205_13::Float64 = ProjectModule.regcoeffs[33, 2]
-  lev305_13::Float64 = ProjectModule.regcoeffs[34, 2]
-  lev1515_13::Float64 = ProjectModule.regcoeffs[35, 2]
-  lev2515_13::Float64 = ProjectModule.regcoeffs[36, 2]
-  lev3515_13::Float64 = ProjectModule.regcoeffs[37, 2]
-  lev11525_13::Float64 = ProjectModule.regcoeffs[38, 2]
-  lev21525_13::Float64 = ProjectModule.regcoeffs[39, 2]
-  lev31525_13::Float64 = ProjectModule.regcoeffs[40, 2]
-  cons_13::Float64 = ProjectModule.regcoeffs[41, 2]
+  const lev105_13::Float64 = ProjectModule.regcoeffs[32, 2]
+  const lev205_13::Float64 = ProjectModule.regcoeffs[33, 2]
+  const lev305_13::Float64 = ProjectModule.regcoeffs[34, 2]
+  const lev1515_13::Float64 = ProjectModule.regcoeffs[35, 2]
+  const lev2515_13::Float64 = ProjectModule.regcoeffs[36, 2]
+  const lev3515_13::Float64 = ProjectModule.regcoeffs[37, 2]
+  const lev11525_13::Float64 = ProjectModule.regcoeffs[38, 2]
+  const lev21525_13::Float64 = ProjectModule.regcoeffs[39, 2]
+  const lev31525_13::Float64 = ProjectModule.regcoeffs[40, 2]
+  const cons_13::Float64 = ProjectModule.regcoeffs[41, 2]
   # Casevars 12
-  lev105_12::Float64 = ProjectModule.regcoeffs[42, 2]
-  lev205_12::Float64 = ProjectModule.regcoeffs[43, 2]
-  lev305_12::Float64 = ProjectModule.regcoeffs[44, 2]
-  lev1515_12::Float64 = ProjectModule.regcoeffs[45, 2]
-  lev2515_12::Float64 = ProjectModule.regcoeffs[46, 2]
-  lev3515_12::Float64 = ProjectModule.regcoeffs[47, 2]
-  lev11525_12::Float64 = ProjectModule.regcoeffs[48, 2]
-  lev21525_12::Float64 = ProjectModule.regcoeffs[49, 2]
-  lev31525_12::Float64 = ProjectModule.regcoeffs[50, 2]
-  cons_12::Float64 = ProjectModule.regcoeffs[51, 2]
+  const lev105_12::Float64 = ProjectModule.regcoeffs[42, 2]
+  const lev205_12::Float64 = ProjectModule.regcoeffs[43, 2]
+  const lev305_12::Float64 = ProjectModule.regcoeffs[44, 2]
+  const lev1515_12::Float64 = ProjectModule.regcoeffs[45, 2]
+  const lev2515_12::Float64 = ProjectModule.regcoeffs[46, 2]
+  const lev3515_12::Float64 = ProjectModule.regcoeffs[47, 2]
+  const lev11525_12::Float64 = ProjectModule.regcoeffs[48, 2]
+  const lev21525_12::Float64 = ProjectModule.regcoeffs[49, 2]
+  const lev31525_12::Float64 = ProjectModule.regcoeffs[50, 2]
+  const cons_12::Float64 = ProjectModule.regcoeffs[51, 2]
   # Casevars 32
-  lev105_32::Float64 = ProjectModule.regcoeffs[52, 2]
-  lev205_32::Float64 = ProjectModule.regcoeffs[53, 2]
-  lev305_32::Float64 = ProjectModule.regcoeffs[54, 2]
-  lev1515_32::Float64 = ProjectModule.regcoeffs[55, 2]
-  lev2515_32::Float64 = ProjectModule.regcoeffs[56, 2]
-  lev3515_32::Float64 = ProjectModule.regcoeffs[57, 2]
-  lev11525_32::Float64 = ProjectModule.regcoeffs[58, 2]
-  lev21525_32::Float64 = ProjectModule.regcoeffs[59, 2]
-  lev31525_32::Float64 = ProjectModule.regcoeffs[60, 2]
-  cons_32::Float64 = ProjectModule.regcoeffs[61, 2]
+  const lev105_32::Float64 = ProjectModule.regcoeffs[52, 2]
+  const lev205_32::Float64 = ProjectModule.regcoeffs[53, 2]
+  const lev305_32::Float64 = ProjectModule.regcoeffs[54, 2]
+  const lev1515_32::Float64 = ProjectModule.regcoeffs[55, 2]
+  const lev2515_32::Float64 = ProjectModule.regcoeffs[56, 2]
+  const lev3515_32::Float64 = ProjectModule.regcoeffs[57, 2]
+  const lev11525_32::Float64 = ProjectModule.regcoeffs[58, 2]
+  const lev21525_32::Float64 = ProjectModule.regcoeffs[59, 2]
+  const lev31525_32::Float64 = ProjectModule.regcoeffs[60, 2]
+  const cons_32::Float64 = ProjectModule.regcoeffs[61, 2]
   # Casevars 31
-  lev105_31::Float64 = ProjectModule.regcoeffs[62, 2]
-  lev205_31::Float64 = ProjectModule.regcoeffs[63, 2]
-  lev305_31::Float64 = ProjectModule.regcoeffs[64, 2]
-  lev1515_31::Float64 = ProjectModule.regcoeffs[65, 2]
-  lev2515_31::Float64 = ProjectModule.regcoeffs[66, 2]
-  lev3515_31::Float64 = ProjectModule.regcoeffs[67, 2]
-  lev11525_31::Float64 = ProjectModule.regcoeffs[68, 2]
-  lev21525_31::Float64 = ProjectModule.regcoeffs[69, 2]
-  lev31525_31::Float64 = ProjectModule.regcoeffs[70, 2]
-  cons_31::Float64 = ProjectModule.regcoeffs[71, 2]
+  const lev105_31::Float64 = ProjectModule.regcoeffs[62, 2]
+  const lev205_31::Float64 = ProjectModule.regcoeffs[63, 2]
+  const lev305_31::Float64 = ProjectModule.regcoeffs[64, 2]
+  const lev1515_31::Float64 = ProjectModule.regcoeffs[65, 2]
+  const lev2515_31::Float64 = ProjectModule.regcoeffs[66, 2]
+  const lev3515_31::Float64 = ProjectModule.regcoeffs[67, 2]
+  const lev11525_31::Float64 = ProjectModule.regcoeffs[68, 2]
+  const lev21525_31::Float64 = ProjectModule.regcoeffs[69, 2]
+  const lev31525_31::Float64 = ProjectModule.regcoeffs[70, 2]
+  const cons_31::Float64 = ProjectModule.regcoeffs[71, 2]
   # Casevars 21
-  lev105_21::Float64 = ProjectModule.regcoeffs[72, 2]
-  lev205_21::Float64 = ProjectModule.regcoeffs[73, 2]
-  lev305_21::Float64 = ProjectModule.regcoeffs[74, 2]
-  lev1515_21::Float64 = ProjectModule.regcoeffs[75, 2]
-  lev2515_21::Float64 = ProjectModule.regcoeffs[76, 2]
-  lev3515_21::Float64 = ProjectModule.regcoeffs[77, 2]
-  lev11525_21::Float64 = ProjectModule.regcoeffs[78, 2]
-  lev21525_21::Float64 = ProjectModule.regcoeffs[79, 2]
-  lev31525_21::Float64 = ProjectModule.regcoeffs[80, 2]
-  cons_21::Float64 = ProjectModule.regcoeffs[81, 2]
+  const lev105_21::Float64 = ProjectModule.regcoeffs[72, 2]
+  const lev205_21::Float64 = ProjectModule.regcoeffs[73, 2]
+  const lev305_21::Float64 = ProjectModule.regcoeffs[74, 2]
+  const lev1515_21::Float64 = ProjectModule.regcoeffs[75, 2]
+  const lev2515_21::Float64 = ProjectModule.regcoeffs[76, 2]
+  const lev3515_21::Float64 = ProjectModule.regcoeffs[77, 2]
+  const lev11525_21::Float64 = ProjectModule.regcoeffs[78, 2]
+  const lev21525_21::Float64 = ProjectModule.regcoeffs[79, 2]
+  const lev31525_21::Float64 = ProjectModule.regcoeffs[80, 2]
+  const cons_21::Float64 = ProjectModule.regcoeffs[81, 2]
   # Casevars 23
-  lev105_23::Float64 = ProjectModule.regcoeffs[82, 2]
-  lev205_23::Float64 = ProjectModule.regcoeffs[83, 2]
-  lev305_23::Float64 = ProjectModule.regcoeffs[84, 2]
-  lev1515_23::Float64 = ProjectModule.regcoeffs[85, 2]
-  lev2515_23::Float64 = ProjectModule.regcoeffs[86, 2]
-  lev3515_23::Float64 = ProjectModule.regcoeffs[87, 2]
-  lev11525_23::Float64 = ProjectModule.regcoeffs[88, 2]
-  lev21525_23::Float64 = ProjectModule.regcoeffs[89, 2]
-  lev31525_23::Float64 = ProjectModule.regcoeffs[90, 2]
-  cons_23::Float64 = ProjectModule.regcoeffs[91, 2]
+  const lev105_23::Float64 = ProjectModule.regcoeffs[82, 2]
+  const lev205_23::Float64 = ProjectModule.regcoeffs[83, 2]
+  const lev305_23::Float64 = ProjectModule.regcoeffs[84, 2]
+  const lev1515_23::Float64 = ProjectModule.regcoeffs[85, 2]
+  const lev2515_23::Float64 = ProjectModule.regcoeffs[86, 2]
+  const lev3515_23::Float64 = ProjectModule.regcoeffs[87, 2]
+  const lev11525_23::Float64 = ProjectModule.regcoeffs[88, 2]
+  const lev21525_23::Float64 = ProjectModule.regcoeffs[89, 2]
+  const lev31525_23::Float64 = ProjectModule.regcoeffs[90, 2]
+  const cons_23::Float64 = ProjectModule.regcoeffs[91, 2]
   # casevars exit
-  lev105_EX::Float64 = ProjectModule.regcoeffs[92, 2]
-  lev205_EX::Float64 = ProjectModule.regcoeffs[93, 2]
-  lev305_EX::Float64 = ProjectModule.regcoeffs[94, 2]
-  lev1515_EX::Float64 = ProjectModule.regcoeffs[95, 2]
-  lev2515_EX::Float64 = ProjectModule.regcoeffs[96, 2]
-  lev3515_EX::Float64 = ProjectModule.regcoeffs[97, 2]
-  lev11525_EX::Float64 = ProjectModule.regcoeffs[98, 2]
-  lev21525_EX::Float64 = ProjectModule.regcoeffs[99, 2]
-  lev31525_EX::Float64 = ProjectModule.regcoeffs[100, 2]
-  cons_EX::Float64 = ProjectModule.regcoeffs[101, 2]
-  case13::Array{Float64,2} = [lev105_13 lev205_13 lev305_13 lev1515_13 lev2515_13 lev3515_13 lev11525_13 lev21525_13 lev31525_13 cons_13]
-  case12::Array{Float64,2} = [lev105_12 lev205_12 lev305_12 lev1515_12 lev2515_12 lev3515_12 lev11525_12 lev21525_12 lev31525_12 cons_12]
-  case32::Array{Float64,2} = [lev105_32 lev205_32 lev305_32 lev1515_32 lev2515_32 lev3515_32 lev11525_32 lev21525_32 lev31525_32 cons_32]
-  case31::Array{Float64,2} = [lev105_31 lev205_31 lev305_31 lev1515_31 lev2515_31 lev3515_31 lev11525_31 lev21525_31 lev31525_31 cons_31]
-  case21::Array{Float64,2} = [lev105_21 lev205_21 lev305_21 lev1515_21 lev2515_21 lev3515_21 lev11525_21 lev21525_21 lev31525_21 cons_21]
-  case23::Array{Float64,2} = [lev105_23 lev205_23 lev305_23 lev1515_23 lev2515_23 lev3515_23 lev11525_23 lev21525_23 lev31525_23 cons_23]
-  caseEX::Array{Float64,2} = [lev105_EX lev205_EX lev305_EX lev1515_EX lev2515_EX lev3515_EX lev11525_EX lev21525_EX lev31525_EX cons_EX]
+  const lev105_EX::Float64 = ProjectModule.regcoeffs[92, 2]
+  const lev205_EX::Float64 = ProjectModule.regcoeffs[93, 2]
+  const lev305_EX::Float64 = ProjectModule.regcoeffs[94, 2]
+  const lev1515_EX::Float64 = ProjectModule.regcoeffs[95, 2]
+  const lev2515_EX::Float64 = ProjectModule.regcoeffs[96, 2]
+  const lev3515_EX::Float64 = ProjectModule.regcoeffs[97, 2]
+  const lev11525_EX::Float64 = ProjectModule.regcoeffs[98, 2]
+  const lev21525_EX::Float64 = ProjectModule.regcoeffs[99, 2]
+  const lev31525_EX::Float64 = ProjectModule.regcoeffs[100, 2]
+  const cons_EX::Float64 = ProjectModule.regcoeffs[101, 2]
+  # collecting...
+  const case13::Array{Float64,2} = [lev105_13 lev205_13 lev305_13 lev1515_13 lev2515_13 lev3515_13 lev11525_13 lev21525_13 lev31525_13 cons_13]
+  const case12::Array{Float64,2} = [lev105_12 lev205_12 lev305_12 lev1515_12 lev2515_12 lev3515_12 lev11525_12 lev21525_12 lev31525_12 cons_12]
+  const case32::Array{Float64,2} = [lev105_32 lev205_32 lev305_32 lev1515_32 lev2515_32 lev3515_32 lev11525_32 lev21525_32 lev31525_32 cons_32]
+  const case31::Array{Float64,2} = [lev105_31 lev205_31 lev305_31 lev1515_31 lev2515_31 lev3515_31 lev11525_31 lev21525_31 lev31525_31 cons_31]
+  const case21::Array{Float64,2} = [lev105_21 lev205_21 lev305_21 lev1515_21 lev2515_21 lev3515_21 lev11525_21 lev21525_21 lev31525_21 cons_21]
+  const case23::Array{Float64,2} = [lev105_23 lev205_23 lev305_23 lev1515_23 lev2515_23 lev3515_23 lev11525_23 lev21525_23 lev31525_23 cons_23]
+  const caseEX::Array{Float64,2} = [lev105_EX lev205_EX lev305_EX lev1515_EX lev2515_EX lev3515_EX lev11525_EX lev21525_EX lev31525_EX cons_EX]
+
   if ownlev == (0,0) #level 1
     # choices - continue, 12, 13, ex
     retst1 = states1(lev1, lev2, lev3)  
