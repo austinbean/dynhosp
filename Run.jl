665e0fa09e00f56ee58d7cc0fba1ad6dc8b725b3
Austin Bean <austinbean@gmail.com>
2017-07-19 14:22:10 -0500

    adding location info and start and end times

--- Run.jl
+++ Run.jl
@@ -3,20 +3,19 @@
 #addprocs(30) # will work on Amazon.
 #addprocs() # will work on home machine.
 addprocs(68) # will work on Stampede
-
 loc = pwd()
-
+println("current location: ", loc)
 using ProjectModule
 
 ds1 = Dates.format(now(), "yyyy-mm-dd-HH-MM-SS")
-println(ds1)
+println("Sim Starting at: ", ds1)
 
 res1, res2 = CombinedSim(68; T1 = 40)
 
 println("Estimation Done - Saving")
 
 ds = Dates.format(now(), "yyyy-mm-dd-HH-MM-SS")
-println(ds)
+println("Finished at: ", ds)
 
 writecsv(loc*"/longobjective"*ds*".csv", res1)
 writecsv(loc*"/shortobjective"*ds*".csv", res2)
