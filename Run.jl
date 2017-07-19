b282fa834682c1a4b6ebf2b5e2a3133bc2126565
Austin Bean <austinbean@gmail.com>
2017-07-19 11:47:28 -0500

    changes to match stampede

--- Run.jl
+++ Run.jl
@@ -8,12 +8,15 @@ loc = pwd()
 
 using ProjectModule
 
+ds1 = Dates.format(now(), "yyyy-mm-dd-HH-MM-SS")
+println(ds1)
 
 res1, res2 = CombinedSim(68; T1 = 40)
 
 println("Estimation Done - Saving")
 
 ds = Dates.format(now(), "yyyy-mm-dd-HH-MM-SS")
+println(ds)
 
 writecsv(loc*"/longobjective"*ds*".csv", res1)
 writecsv(loc*"/shortobjective"*ds*".csv", res2)
