607f07e22041732c31b036b00660535d248a0965
Austin Bean <austinbean@gmail.com>
2017-08-13 17:53:14 -0500

    continuation value going negative - related to Appchoice

--- NewApprox.jl
+++ NewApprox.jl
@@ -12,8 +12,7 @@ dyn = CounterObjects(5);
 p1 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
 p2 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
 ch2 = [11] # larger market. 
-out2 = Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }()
-NewApprox(dyn, ch2, p1, p2; itlim = 20_000, outvals = out2)
+NewApprox(dyn, ch2, p1, p2; itlim = 20_000)
 
 
 """
@@ -21,12 +20,9 @@ function NewApprox(D::DynState,
                    chunk::Array{Int64,1}, 
                    p1::patientcount,
                    p2::patientcount;
-                   itlim::Int64 = 1000,
-                   outvals::Dict{ Int64, Dict{NTuple{10, Int64},  Float64} } = Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }())
-  tempvals::Dict{ Int64, Dict{NTuple{10, Int64}, Float64}  } = Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }()
+                   itlim::Int64 = 100_000)
+  outvals::Dict{ Int64, Dict{NTuple{10, Int64},  Float64} } = Dict{ Int64, Dict{NTuple{10, Int64}, Float64 } }()
   tracker::Dict{NTuple{10, Int64}, Int64} = Dict{NTuple{10, Int64}, Int64}()            # Dict to hold visited states
-  DictClean(tempvals)                                                                   # initialize to zero. 
-  totest::Dict{Int64,Bool} = Dict{Int64,Bool}()                                         # will record convergence (Fid, Bool) Dict.
   all_locs::Dict{Int64,Int64} = Dict{Int64, Int64}()                                    # will record the locations of competitors (Fid, Loc in Dyn.all) Dict, NOT the firm itself.  
   # Or return a tuple in GiveState.
   st_dict::Dict{Int64,NTuple{9,Int64}} = Dict{Int64,NTuple{9,Int64}}()                  # will record the states of all firms from the point of view of el.
@@ -37,66 +33,44 @@ function NewApprox(D::DynState,
     FindComps(D, neighbors, D.all[el])                                                  # these are addresses of competitors in D.all 
     NFids(D, nfds, D.all[el])                                                           # records the fids of neighbors only, as fids.  
     push!(neighbors, el)                                                                # add the location of the firm in chunk
-    if !haskey(outvals, D.all[el].fid)                                                  # add an empty dict IF there isn't already an entry.
-      outvals[D.all[el].fid] = Dict{NTuple{10, Int64}, Float64 }()
-    end 
-    tempvals[D.all[el].fid] = Dict{NTuple{10, Int64}, Float64 }()
+    outvals[D.all[el].fid] = Dict{NTuple{10, Int64}, Float64 }()
     CompsDict(neighbors, D, all_locs)                                                   # now this is Dict{Fid, Location}
-    StateRecord(all_locs, D, st_dict)                                                   # returns the restricted state.
-    for el2 in neighbors                                                                # adds keys for the neighbors to the temp dict. 
-      outvals[D.all[el2].fid] = Dict{NTuple{10, Int64}, Float64}()
-      tempvals[D.all[el2].fid] = Dict{NTuple{10, Int64},Float64}() 
-      totest[D.all[el2].fid] = false                                                    # initialized to FALSE - not converged. 
-      StateEnumerate(TupletoCNS(st_dict[D.all[el2].fid]), outvals[D.all[el2].fid]) 
-      StateEnumerate(TupletoCNS(st_dict[D.all[el2].fid]), tempvals[D.all[el2].fid])
-    end 
-    if !haskey(outvals, D.all[el].fid)
-      StateEnumerate(D.all[el].cns, outvals[D.all[el].fid])
-      StateEnumerate(D.all[el].cns, tempvals[D.all[el].fid])                            # this does NOT need starting values.  
-    end 
-    if haskey(outvals, D.all[el].fid)
-      DictCopy(tempvals, outvals, 1.0)                                                  # if there is an entry for the value, copy FROM outvals TO tempvals.  
-    end                                
-    totest[D.all[el].fid] = false                                                       # all facilities to do initially set to false.  
+    StateRecord(all_locs, D, st_dict)                                                   # returns the restricted state. 
+    StateEnumerate(D.all[el].cns, outvals[D.all[el].fid])
   end
   altstates = MakeStateBlock(nfds)                                                      # generates a list of states to try, e.g., entry, exit and levels for each possible competitor.  
   converge::Bool = true
   inv_costs = zeros(9)
-  wgts = zeros(outvals[D.all[chunk[1]].fid].count)                   # this will hold the weights.  
-  elts = Array{NTuple{10,Int64}}(outvals[D.all[chunk[1]].fid].count) # this will hold the states
-  dists = RecordDists(D, chunk, all_locs)                         # for each neighbor, record the distance to the main firm.  Do this once.
+  wgts = zeros(outvals[D.all[chunk[1]].fid].count)                                      # this will hold the weights.  
+  elts = Array{NTuple{10,Int64}}(outvals[D.all[chunk[1]].fid].count)                    # this will hold the states
+  dists = RecordDists(D, chunk, all_locs)                                               # for each neighbor, record the distance to the main firm.  Do this once.
   statehold = zeros(Int64,9)
   k::Int64 = D.all[chunk[1]].fid 
   while (converge)&(its<itlim)                                                          # if true keep going.  
-    # need a counter for iterations.
-    nextstate = DictRandomize(outvals[k], elts, wgts)
+    nextstate = DictRandomize(outvals[k], elts, wgts)                                   # generate the next state.
     if haskey(tracker, nextstate)
       tracker[nextstate] += 1
     else 
       tracker[nextstate] = 1
     end 
     r = MakeConfig(nextstate, dists, altstates, statehold)
+    # TODO - for what is the next line used?  
     st_dict[k] = GiveState(D, chunk, all_locs, altstates[r,:], D.all[all_locs[k]].cns) 
     MapCompState(D, all_locs, chunk, FindFids(D, chunk), altstates[r,:])
     InvCosts(st_dict[k], false, inv_costs)
-    AppChoice(tempvals, outvals, all_locs, st_dict, k, all_locs[k], p1, p2, D, inv_costs; counter = false)
-    ResetCompState(D, all_locs, chunk, FindFids(D, chunk), altstates[r,:]) # set it back 
-    # need a new convergence check too.  
-    #ExactConvergence(tempvals, outvals, totest, its; messages = false)   
-    if its %1_000 == 0
-      InexactConvergence(D, chunk, p1, p2, tracker, outvals, 100)
-      #ResetTracker(tracker) # TODO - uncomment when ready.  
+    outvals[k][nextstate] = (1-(1/(1+tracker[nextstate])))*(outvals[k][nextstate]) + (1/(1+tracker[nextstate]))*(AppChoice(all_locs[k], k, p1, p2, D, inv_costs)) # only map the state out to one element of outvals.
+    println("its: ", its, " min: ", OutMin(outvals, tracker, k))
+    ResetCompState(D, all_locs, chunk, FindFids(D, chunk), altstates[r,:])              # set it back 
+    if its %10_000 == 0
+      cv = InexactConvergence(D, chunk, p1, p2, tracker, outvals, 100)
+      if cv < 1e-6
+        converge = false # stop.
+      end 
+      ResetTracker(tracker)  
       println("iteration: ", its)
-      println("minimum: ", CheckMin(outvals, tempvals)) # NB:  this doesn't really compute a sensible comparison in this case, I think.
     end 
-    # Copy the values and clean up.
-    DictCopy(outvals, tempvals, 1/(its+1))                                            # NB - weight placed on new vs. old values.  
-    DictClean(tempvals)                                                               # sets up for rewriting.
-    for ky1 in keys(totest)                                                           # this tests every facility every time, but that's ok. 
-      converge = ConvTest(totest)                                                     # iterates over bools in totest returns product
-    end   
     its += 1
   end 
   # Return equilibrium values:
-  return outvals, tracker
+  return outvals
 end
