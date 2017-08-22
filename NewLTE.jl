2891ba3e9ae2e6dab28278364da65d09969671b1
Austin Bean <austinbean@gmail.com>
2017-08-22 17:30:44 -0500

    added additional results

--- NewLTE.jl
+++ NewLTE.jl
@@ -180,6 +180,7 @@ println("Count of Underflow ", undercounter)
 ans1 = mean(sim_vals,1);
 guess = [1000.0, 100.0, 10.0, 12038.0, 12038, 12038, 66143, 66143, 66143, 19799, 19799, 19799, 4044, 4044, 4044, 6242, 6242, 6242, 1329, 1329, 1329, 412, 412, 412, 2000000, 5000000, 0, -100000, 2000000, 0, -200000, -100000, 0 ];
 ResultsPrint(ans1, guess)
+Intervals(sim_vals)
 
 
 # Plotting Results:
