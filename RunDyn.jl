fd4e538e052d5f77710e4a7a0f7961024e43afae
Austin Bean <austinbean@gmail.com>
2017-08-25 17:37:55 -0500

    Ready to run

--- RunDyn.jl
+++ RunDyn.jl
@@ -1,8 +1,9 @@
 # Runs the dynamic computation
 # Note the parameters wallh and wallm in ExactControl - these determine how long the simulation should take.  
 
-#addprocs(68) # will work on Stampede
-addprocs(2) # for testing at home.
+addprocs(68) # will work on Stampede
+
+#addprocs(2) # for testing at home.
 
 
 loc = pwd()
@@ -15,7 +16,7 @@ println("Sim Starting at: ", ds1)
 
 dyn = CounterObjects(5);
 res1 = Dict{Int64,Dict{NTuple{10,Int64},Float64}}()
-ExactControl(dyn, 0, 5; results = res1)
+ExactControl(dyn, 4, 15; results = res1)
 
 
 outp = ResultsWrite(res1)
@@ -25,7 +26,7 @@ ds = Dates.format(now(), "yyyy-mm-dd-HH-MM-SS")
 
 println("Finished at: ", ds)
 
-writecsv(loc*"/longobjective"*ds*".csv", outp)
+writecsv(loc*"/dynresults"*ds*".csv", outp)
 
 println("Done Saving")
 
