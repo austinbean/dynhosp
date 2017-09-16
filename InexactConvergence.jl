c68d8a139d432be1c45372944c4a0f1bd34d56b6
Austin Bean <austinbean@gmail.com>
2017-09-16 14:07:40 -0500

    fixed bounds error but don't know where it was from

--- InexactConvergence.jl
+++ InexactConvergence.jl
@@ -1,50 +1,18 @@
 """
-`InexactConvergence(D::DynState,
-                    chunk::Array{Int64,1}, 
-                    p1::patientcount,
-                    p2::patientcount,
-                    tracker::Dict{NTuple{10, Int64}, Int64}, 
-                    outvals::Dict{Int64, Dict{NTuple{10, Int64},Float64}},
-                    itlim::Int64)`
-
-# generates the tracker to test with InexactConvergence
-As input take the output of NewApprox.
-function tgen(d1)
-  tr = Dict{NTuple{10, Int64}, Int64}()
-  for k1 in keys(d1)
-    for k2 in keys(d1[k1])
-      tr[k2] = 100
-    end 
-  end 
-  return tr 
-end 
-dyn = CounterObjects(1);
-ddd = NewApprox(dyn, [37], p1, p2; wlh = 0, wlm = 10, itlim = 100)
-p1 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-p2 = patientcount(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
-tr1 = tgen(ddd)
-
-InexactConvergence(dyn, [37], p1, p2,  tr1, ddd, 10; 0, 6)
-
-Slowdown here comes from |states|×|itlim| iterations.
-
-InexactConvergence(dyn, [37], p1, p2, tr1, ab, 10) 138.363867 seconds (349.86 M allocations: 24.028 GiB, 1.82% gc time)
-
-InexactConvergence(dyn, [37], p1, p2, tr1, ab, 100) 1425.038687 seconds (3.50 G allocations: 240.145 GiB, 1.81% gc time)
-
+`InexactConvergence(`
 """
 function InexactConvergence(D::DynState,
-                            chunk::Array{Int64,1}, 
+                            chunk::Array{Int64,1},
                             p1::patientcount,
                             p2::patientcount,
                             tracker::Dict{NTuple{10, Int64}, Int64}, 
                             outvals::Dict{Int64, Dict{NTuple{10, Int64},Float64}},
-                            itlim::Int64;
+                            itlim::Int64; 
                             wh::Int64 = 100, 
                             wm::Int64 = 0)
     strt = now()
-    wl = Dates.Millisecond(Dates.Hour(wlh)) + Dates.Millisecond(Dates.Minute(wlm)) - Dates.Millisecond(Dates.Minute(5))
-    approxvals::Dict{NTuple{10, Int64},Float64} =Dict{NTuple{10, Int64},Float64}()        # put results of approximation here.  
+    wl = Dates.Millisecond(Dates.Hour(wh)) + Dates.Millisecond(Dates.Minute(wm)) - Dates.Millisecond(Dates.Minute(5))
+    approxvals::Dict{NTuple{10, Int64},Float64} = Dict{NTuple{10, Int64},Float64}()       # put results of approximation here.  
     all_locs::Dict{Int64,Int64} = Dict{Int64, Int64}()                                    # will record the locations of competitors (Fid, Loc in Dyn.all) Dict, NOT the firm itself.  
     st_dict::Dict{Int64,NTuple{9,Int64}} = Dict{Int64,NTuple{9,Int64}}()                  # will record the states of all firms from the point of view of el.
     neighbors::Array{Int64,1} = Array{Int64,1}()                                          # will record the locations in D.all[] of competing firms AND the firm itself.
@@ -95,6 +63,6 @@ function InexactConvergence(D::DynState,
     for k1 in keys(tracker)
         test += tracker[k1]*(outvals[k][k1] - approxvals[k1])^2
     end 
-    return test/totv
+    return nothing #test/totv
 end 
 
