147396b85e5aa6a0d156a9b1dd5a87f54139c80b
Austin Bean <austinbean@gmail.com>
2018-09-28 16:07:26 -0400

    re-enabled saving, changed imports, exports

--- Run.jl
+++ Run.jl
@@ -1,17 +1,18 @@
 
 using Distributed 
+using Dates
 #addprocs(30) # will work on Amazon.
 addprocs() # will work on home machine.
 #addprocs(68) # will work on Stampede
 loc = pwd()
-println("current location: ", loc)
-@everywhere push!(LOAD_PATH, loc)
+#println("current location: ", loc)
+@everywhere push!(LOAD_PATH, $loc)
 @everywhere using ProjectModule
 
 ds1 = Dates.format(Dates.now(), "yyyy-mm-dd-HH-MM-SS")
 println("Sim Starting at: ", ds1)
 
-res1, res2 = CombinedSim(468; T1 = 50) # 476/68 = 7 reps/core.  468/18 cores at HCMG
+res1, res2 = CombinedSim(6; T1 = 1) # 476/68 = 7 reps/core.  468/18 cores at HCMG
 
 println("Estimation Done - Saving")
 println("size? ", size(res1), " ", size(res2))
@@ -23,8 +24,8 @@ println("Finished at: ", ds)
 println("locs: ", loc*"/longobjective"*ds*".csv")
 println("locs2: ", loc*"/shortobjective"*ds*".csv")
 
-#writecsv(loc*"/longobjective"*ds*".csv", res1)
-#writecsv(loc*"/shortobjective"*ds*".csv", res2)
+ProjectModule.writedlm(loc*"/longobjective"*ds*".csv", res1, ",")
+ProjectModule.writedlm(loc*"/shortobjective"*ds*".csv", res2, ",")
 
 
 println("Done Saving")
